d4919398ba94bffc69c7aa93c05460d34aef1183
jeroenransijn <jssrdesign@gmail.com>
2017-08-20 21:20:35 -0700

    scaffold stories imports added

--- tools/component-stories-template.js
+++ tools/component-stories-template.js
@@ -2,7 +2,7 @@ export default function storiesTemplate({ packageName, componentNames }) {
   const template = []
   template.push(`import { storiesOf } from '@kadira/storybook'`)
   template.push(`import React from 'react'`)
-  template.push(`import ComponentName from '../src'`)
+  template.push(`import { ${componentNames.join(', ')} } from '../src/'`)
   template.push('')
   template.push(`storiesOf('${packageName}', module)`)
   componentNames.forEach(componentName => {
