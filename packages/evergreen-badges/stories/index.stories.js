22aab75359991b9c041c254e5ac373ec1b89c0db
Benjamin <bhoffman8@gmail.com>
2017-09-12 18:05:03 -0700

    adds Pill component

--- packages/evergreen-badges/stories/index.stories.js
+++ packages/evergreen-badges/stories/index.stories.js
@@ -1,19 +1,42 @@
 import { storiesOf } from '@storybook/react'
 import React from 'react'
-import { Badge, BadgeAppearances } from '../src/'
+import { Badge, Pill, BadgeAppearances } from '../src/'
 
-const baseStyles = {
-  margin: 16,
+const wrapperStyles = {
+  display: 'flex',
 }
 
-storiesOf('badges', module).add('Badge', () =>
-  <div>
-    {Object.keys(BadgeAppearances).map(appearance =>
-      <div>
-        <Badge appearance={appearance} {...baseStyles}>
-          {appearance}
-        </Badge>
-      </div>,
-    )}
-  </div>,
-)
+const baseStyles = {
+  margin: 8,
+  display: 'block',
+}
+
+storiesOf('badges', module)
+  .add('Badge', () =>
+    <div style={{ ...wrapperStyles }}>
+      {Object.keys(BadgeAppearances).map(appearance =>
+        <div>
+          <Badge appearance={appearance} {...baseStyles}>
+            {appearance}
+          </Badge>
+          <Badge appearance={appearance} {...baseStyles} isSolid>
+            {appearance}
+          </Badge>
+        </div>,
+      )}
+    </div>,
+  )
+  .add('Pill', () =>
+    <div style={{ ...wrapperStyles }}>
+      {Object.keys(BadgeAppearances).map(appearance =>
+        <div>
+          <Pill appearance={appearance} {...baseStyles}>
+            {appearance}
+          </Pill>
+          <Pill appearance={appearance} {...baseStyles} isSolid="true">
+            {appearance}
+          </Pill>
+        </div>,
+      )}
+    </div>,
+  )
