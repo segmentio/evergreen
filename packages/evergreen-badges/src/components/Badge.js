d72829339e6672b19deeb2e12dfe1c388829a114
benjamin hoffman <6520022+benjaminhoffman@users.noreply.github.com>
2017-10-13 11:36:09 -0700

    adds back isSolid prop

--- packages/evergreen-badges/src/components/Badge.js
+++ packages/evergreen-badges/src/components/Badge.js
@@ -23,6 +23,7 @@ export default class Badge extends PureComponent {
     textDecoration: 'none',
     textTransform: 'uppercase',
     appearance: 'neutral',
+    isSolid: false,
   }
 
   render() {
