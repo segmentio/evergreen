175d05125b6929804ea763c9f3d79c598db375f6
Fouad Matin <fm@fouad.co>
2017-10-17 14:49:55 -0700

    icons: add iconWidth/iconHeight (#31)

--- packages/evergreen-icons/src/components/CloseIcon.js
+++ packages/evergreen-icons/src/components/CloseIcon.js
@@ -3,7 +3,7 @@ import Icon from './Icon'
 
 export default class CloseIcon extends PureComponent {
   render() {
-    const { ...props } = this.props
+    const { iconWidth, iconHeight, ...props } = this.props
     return (
       <Icon {...props}>
         <svg
@@ -11,8 +11,8 @@ export default class CloseIcon extends PureComponent {
           y="0px"
           viewBox="0 0 16 16"
           xmlSpace="preserve"
-          width={16}
-          height={16}
+          width={iconWidth || 16}
+          height={iconHeight || 16}
           fill="currentColor"
         >
           <path d="M14.7,1.3c-0.4-0.4-1-0.4-1.4,0L8,6.6L2.7,1.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4L6.6,8l-5.3,5.3 c-0.4,0.4-0.4,1,0,1.4C1.5,14.9,1.7,15,2,15s0.5-0.1,0.7-0.3L8,9.4l5.3,5.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L9.4,8l5.3-5.3C15.1,2.3,15.1,1.7,14.7,1.3z" />
