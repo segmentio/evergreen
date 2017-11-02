c0f094011e1a28f858b427cc2074481eb6143fcb
Jeroen Ransijn <jssrdesign@gmail.com>
2017-11-01 19:04:36 -0700

    badges fix (#40)

--- packages/evergreen-badges/src/components/Badge.js
+++ packages/evergreen-badges/src/components/Badge.js
@@ -1,24 +1,25 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import { Strong } from 'evergreen-typography'
-import BadgeAppearances from '../styles/badge-appearances'
+import { FillAppearances as BadgeAppearances } from 'evergreen-color-utils'
 
 export default class Badge extends PureComponent {
   static propTypes = {
     ...Text.propTypes,
-    appearance: PropTypes.oneOf(Object.keys(BadgeAppearances.default)).isRequired,
+    appearance: PropTypes.oneOf(Object.keys(BadgeAppearances.default))
+      .isRequired,
   }
 
   static defaultProps = {
     display: 'inline-block',
     boxSizing: 'border-box',
-    height: 24,
+    height: 16,
+    size: 300,
     paddingTop: 0,
-    paddingRight: 4,
+    paddingRight: 6,
     paddingBottom: 0,
-    paddingLeft: 4,
+    paddingLeft: 6,
     borderRadius: 2,
-    fontWeight: 200,
     textAlign: 'center',
     textDecoration: 'none',
     textTransform: 'uppercase',
