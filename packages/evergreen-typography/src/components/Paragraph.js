a0d89f1cb102c49b4ed6e9d630b25bb0efdcbda9
Jeroen Ransijn <jssrdesign@gmail.com>
2017-11-06 21:00:14 -0800

    updated typography (#51)

--- packages/evergreen-typography/src/components/Paragraph.js
+++ packages/evergreen-typography/src/components/Paragraph.js
@@ -1,16 +1,25 @@
 import React, { PureComponent } from 'react'
+import mapValues from 'lodash.mapvalues'
 import Text from './Text'
+import TextStyles from '../styles/TextStyles'
+
+const textStyleTransformation = ({ lineHeight, ...textStyle }) => ({
+  ...textStyle,
+  // Multiply line height by 1.1
+  lineHeight: `${Math.round(parseFloat(lineHeight, 10) * 1.08)}px`,
+})
 
 export default class Paragraph extends PureComponent {
+  static propTypes = {
+    ...Text.propTypes,
+  }
+
   static defaultProps = {
+    ...Text.defaultProps,
     is: 'p',
     marginTop: 0,
-    marginBottom: '1em',
-    textStyleTransformation: ({ lineHeight, ...textStyle }) => ({
-      ...textStyle,
-      // Multiply line height by 1.1
-      lineHeight: `${Math.round(parseFloat(lineHeight, 10) * 1.1)}px`,
-    }),
+    marginBottom: 0,
+    textStyles: mapValues(TextStyles, textStyleTransformation),
   }
 
   render() {
--- packages/evergreen-typography/src/components/Small.js
+++ packages/evergreen-typography/src/components/Small.js
@@ -1,13 +1,22 @@
 import React, { PureComponent } from 'react'
+import mapValues from 'lodash.mapvalues'
 import Text from './Text'
+import TextStyles from '../styles/TextStyles'
+
+const textStyleTransformation = ({ fontSize, ...textStyle }) => ({
+  ...textStyle,
+  fontSize: `${Math.round(parseInt(fontSize, 10) * 0.8)}px`,
+})
 
 export default class Small extends PureComponent {
+  static propTypes = {
+    ...Text.propTypes,
+  }
+
   static defaultProps = {
+    ...Text.defaultProps,
     is: 'small',
-    textStyleTransformation: ({ fontSize, ...textStyle }) => ({
-      ...textStyle,
-      fontSize: `${Math.round(parseInt(fontSize, 10) * 0.8)}px`,
-    }),
+    textStyles: mapValues(TextStyles, textStyleTransformation),
   }
 
   render() {
