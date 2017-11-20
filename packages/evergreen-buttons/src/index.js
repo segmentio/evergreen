a51409c52240af362cabecad7850549034c15af6
jeroenransijn <jssrdesign@gmail.com>
2017-11-20 11:39:18 -0800

    icon buttons + presets

--- packages/evergreen-buttons/src/index.js
+++ packages/evergreen-buttons/src/index.js
@@ -1,5 +1,6 @@
 import Button from './components/Button'
 
+export { default as BackButton } from './components/BackButton'
 export { default as IconButton } from './components/IconButton'
 
 export { default as ButtonAppearances } from './styles/ButtonAppearances'
