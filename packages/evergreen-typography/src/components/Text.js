cdf1bd521e3c6664c251d51b1ed91d091d8d301a
Jeroen Ransijn <jssrdesign@gmail.com>
2017-11-21 15:04:01 -0800

    isUppercase support (#81)

--- packages/evergreen-typography/src/components/Text.js
+++ packages/evergreen-typography/src/components/Text.js
@@ -3,7 +3,10 @@ import PropTypes from 'prop-types'
 import Box from 'ui-box'
 import FontFamilies from '../styles/FontFamilies'
 import TextStyles from '../styles/TextStyles'
+import TextUppercaseStyles from '../styles/TextUppercaseStyles'
 import TextColors from '../styles/TextColors'
+
+const isDev = process.env.NODE_ENV === 'development' || !process.env.NODE_ENV
 
 export default class Text extends PureComponent {
   static propTypes = {
@@ -12,6 +15,8 @@ export default class Text extends PureComponent {
     color: PropTypes.string,
     fontFamily: PropTypes.oneOf(Object.keys(FontFamilies)),
     textStyles: PropTypes.object,
+    textUppercaseStyles: PropTypes.object,
+    isUppercase: PropTypes.bool,
   }
 
   static defaultProps = {
@@ -20,16 +25,41 @@ export default class Text extends PureComponent {
     color: 'default',
     fontFamily: 'ui',
     textStyles: TextStyles,
+    textUppercaseStyles: TextUppercaseStyles,
   }
 
   render() {
-    const { size, color, textStyles, fontFamily, ...props } = this.props
+    const {
+      size,
+      color,
+      textStyles,
+      textUppercaseStyles,
+      fontFamily,
+      isUppercase,
+      ...props
+    } = this.props
+
+    let textStyle = textStyles[size]
+
+    if (isUppercase) {
+      // TODO: Only 100 and 200 support uppercase styles atm
+      // Fallback on non uppercase atm
+      if (Object.prototype.hasOwnProperty.call(textUppercaseStyles, size)) {
+        textStyle = textUppercaseStyles[size]
+      } else if (isDev) {
+        console.error(
+          `Uppercase ${size} not supported. <Text isUppercase> only supports the following sizes: ${JSON.stringify(
+            Object.keys(textUppercaseStyles).map(Number),
+          )}`,
+        )
+      }
+    }
 
     return (
       <Box
         {...(color !== null ? { color: TextColors[color] || color } : {})}
         fontFamily={FontFamilies[fontFamily] || fontFamily}
-        {...textStyles[size]}
+        {...textStyle}
         {...props}
       />
     )
