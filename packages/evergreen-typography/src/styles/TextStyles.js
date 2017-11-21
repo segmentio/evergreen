013537e060a38f29f80e78a8771e7ef67ea8dcb0
Jeroen Ransijn <jssrdesign@gmail.com>
2017-11-21 15:04:01 -0800

    isUppercase support (#81)

--- packages/evergreen-typography/src/styles/TextStyles.js
+++ packages/evergreen-typography/src/styles/TextStyles.js
@@ -39,14 +39,10 @@ export default {
     fontSize: '11px',
     fontWeight: 400,
     lineHeight: '16px',
-    letterSpacing: '0.3px',
-    textTransform: 'uppercase',
   },
   '100': {
     fontSize: '10px',
     fontWeight: 400,
     lineHeight: '16px',
-    letterSpacing: '0.3px',
-    textTransform: 'uppercase',
   },
 }
