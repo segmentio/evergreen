a94c429a88aa50834924100f8d63eba90ee5ba97
jeroenransijn <jssrdesign@gmail.com>
2017-12-05 15:08:51 -0800

    first pass

---
+++ docs/src/components/ComponentReadme.js
@@ -0,0 +1,41 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+
+export default class ComponentReadme extends PureComponent {
+  static propTypes = {
+    packageJSON: PropTypes.object,
+    name: PropTypes.string,
+    designGuidelines: PropTypes.node,
+  }
+
+  constructor(props, context) {
+    super(props, context)
+
+    this.state = {}
+  }
+
+  render() {
+    const { children, packageJSON, designGuidelines, ...props } = this.props
+    console.log(packageJSON)
+    return (
+      <article className="ComponentReadme" {...props}>
+        <div className="ComponentReadme-inner">
+          <header className="ComponentReadme-header">
+            <h1 className="ComponentReadme-title">Buttons</h1>
+            <p className="ComponentReadme-subtitle">
+              This package exports a <code>Button</code> and a{' '}
+              <code>IconButton</code> component
+            </p>
+            <nav className="ComponentReadme-nav">
+              <a href="#" aria-selected="true">
+                Design Guidelines
+              </a>
+              <a href="#">Code & Examples</a>
+            </nav>
+          </header>
+          <div>{React.createElement(designGuidelines)}</div>
+        </div>
+      </article>
+    )
+  }
+}
--- docs/src/templates/component.js
+++ docs/src/templates/component.js
@@ -1,13 +1,56 @@
 import React from 'react'
 import getComponent from '../utils/getComponent'
+import TopBar from '../components/TopBar'
+import Sidebar from '../components/Sidebar'
+import ComponentReadme from '../components/ComponentReadme'
+
+// Mock data for now
+const data = {
+  name: 'Buttons',
+  packageJSON: {
+    name: 'evergreen-buttons',
+    version: '2.18.18',
+    description: 'React components: Button',
+    main: 'lib/index.js',
+    keywords: [
+      'evergreen',
+      'segment',
+      'ui',
+      'react',
+      'Button',
+      'ButtonAppearances',
+    ],
+    author: 'Segment',
+    license: 'MIT',
+    peerDependencies: {
+      'prop-types': '^15.0.0',
+      react: '^16.0.0',
+    },
+    dependencies: {
+      'evergreen-colors': '^2.18.14',
+      'evergreen-icons': '^2.18.14',
+      'evergreen-shared-styles': '^2.18.18',
+      'evergreen-typography': '^2.18.14',
+      'ui-box': '^0.5.4',
+    },
+    devDependencies: {
+      'evergreen-layers': '^2.18.14',
+    },
+  },
+}
 
 export default stuff => {
-  const comp = getComponent('buttons')
-  console.log('stuff', stuff)
+  const docs = getComponent('buttons')
+  console.log('stuff', docs)
   return (
-    <div>
-      <aside>sidebar</aside>
-      <main>{React.createElement(comp)}</main>
+    <div className="MainLayout">
+      <TopBar />
+      <main className="MainLayout-main">
+        <Sidebar />
+        <div className="MainLayout-content">
+          <ComponentReadme {...data} designGuidelines={docs} />
+        </div>
+      </main>
     </div>
   )
 }
