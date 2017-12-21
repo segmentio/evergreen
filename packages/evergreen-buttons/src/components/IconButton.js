1d9a0c13011295ea91d6400c87b52975eedc20f8
jeroenransijn <jssrdesign@gmail.com>
2017-12-20 17:16:51 -0800

    Button documented

--- packages/evergreen-buttons/src/components/IconButton.js
+++ packages/evergreen-buttons/src/components/IconButton.js
@@ -6,6 +6,9 @@ import Button from './Button'
 
 export default class IconButton extends PureComponent {
   static propTypes = {
+    /**
+     * Composes the Button component as the base.
+     */
     ...Button.propTypes,
     iconAim: PropTypes.oneOf(Object.keys(IconAim)).isRequired,
     icon: PropTypes.oneOf(Object.keys(IconMap))
