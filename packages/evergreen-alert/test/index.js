3d694f4470ac6fb38274d34f512db81193be4e66
Roland Warmerdam <roland@segment.com>
2017-12-22 17:24:37 -0800

    Add unit testing setup

---
+++ packages/evergreen-alert/test/index.js
@@ -0,0 +1,62 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react'
+import test from 'ava'
+import render from 'react-test-renderer'
+import { shallow, configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import { CheckCircleIcon } from 'evergreen-icons'
+import { Alert } from '../src'
+
+configure({ adapter: new Adapter() })
+
+test('alert: basic snapshot', t => {
+  const component = <Alert title="A simple general message" />
+  const tree = render.create(component).toJSON()
+  t.snapshot(tree)
+})
+
+test('alert: outputs title', t => {
+  const component = shallow(<Alert title="Test title" />)
+  t.true(component.contains('Test title'))
+})
+
+test('alert: outputs children', t => {
+  const component = shallow(<Alert title="Test title">Test content</Alert>)
+  t.true(component.contains('Test content'))
+})
+
+test('alert: type snapshot', t => {
+  const component = <Alert title="Test title" type="danger" />
+  const tree = render.create(component).toJSON()
+  t.snapshot(tree)
+})
+
+test('alert: hasTrim=true snapshot', t => {
+  const component = <Alert title="Test title" hasTrim />
+  const tree = render.create(component).toJSON()
+  t.snapshot(tree)
+})
+
+test('alert: hasTrim=false snapshot', t => {
+  const component = <Alert title="Test title" hasTrim={false} />
+  const tree = render.create(component).toJSON()
+  t.snapshot(tree)
+})
+
+test('alert: outputs icon (hasIcon=true)', t => {
+  const component = shallow(<Alert title="Test title" type="success" hasIcon />)
+  t.true(component.containsMatchingElement(<CheckCircleIcon />))
+})
+
+test('alert: does not output icon (hasIcon=false)', t => {
+  const component = shallow(
+    <Alert title="Test title" type="success" hasIcon={false} />
+  )
+  t.false(component.containsMatchingElement(<CheckCircleIcon />))
+})
+
+test('alert: appearance snapshot', t => {
+  const component = <Alert title="Test title" appearance="card" />
+  const tree = render.create(component).toJSON()
+  t.snapshot(tree)
+})
