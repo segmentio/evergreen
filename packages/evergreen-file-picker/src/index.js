551241a578cab96350d21f8642ae0be49a33e43a
Roland Warmerdam <rowno@webspirited.com>
2018-01-18 10:54:11 -0800

    Add file-picker component (#109)
    * Add file-picker component
    
    * Remove unused files
    
    * Add disabled styles and use t.true()
    
    * Tweak disabled style

---
+++ packages/evergreen-file-picker/src/index.js
@@ -0,0 +1,4 @@
+import FilePicker, { CLASS_PREFIX } from './components/FilePicker'
+
+export default FilePicker
+export { FilePicker, CLASS_PREFIX }
---
+++ packages/evergreen-file-picker/stories/index.stories.js
@@ -0,0 +1,19 @@
+import { storiesOf } from '@storybook/react' // eslint-disable-line import/no-extraneous-dependencies
+import React from 'react'
+import Box from 'ui-box'
+import { FilePicker } from '../src/'
+
+storiesOf('file-picker', module).add('FilePicker', () => (
+  <Box padding={40}>
+    {(() => {
+      document.body.style.margin = '0'
+      document.body.style.height = '100vh'
+    })()}
+
+    <FilePicker multiple width={250} marginBottom={32} />
+
+    <FilePicker multiple width={350} height={24} marginBottom={32} />
+
+    <FilePicker disabled width={250} marginBottom={32} />
+  </Box>
+))
