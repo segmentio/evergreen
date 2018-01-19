05bbf0667b66b3cb63ce00643f2bd585798ac325
Roland Warmerdam <rowno@webspirited.com>
2018-01-19 10:14:17 -0800

    avatar: protect against repeated spaces (#111)

--- packages/evergreen-avatar/src/utils/getInitials.js
+++ packages/evergreen-avatar/src/utils/getInitials.js
@@ -2,8 +2,8 @@ export default function getInitials(name, fallback = '?') {
   if (!name || typeof name !== 'string') return fallback
   return name
     .replace(/\s+/, ' ')
-    .split(' ')
+    .split(' ') // Repeated spaces results in empty strings
     .slice(0, 2)
-    .map(v => v[0].toUpperCase())
+    .map(v => v && v[0].toUpperCase()) // Watch out for empty strings
     .join('')
 }
