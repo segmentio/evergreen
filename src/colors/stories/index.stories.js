e10f90394aee92541ca2709033e778e586e4dc64
jeroenransijn <jssrdesign@gmail.com>
2018-07-09 16:26:13 -0700

    color mapping example

--- src/colors/stories/index.stories.js
+++ src/colors/stories/index.stories.js
@@ -1,9 +1,12 @@
 import { storiesOf } from '@storybook/react'
 import React from 'react'
 import ColorExamples from '../docs/ColorExamples'
+import ColorMapping from './ColorMapping'
 
-storiesOf('colors', module).add('overview', () => (
-  <div style={{ margin: 40 }}>
-    <ColorExamples />
-  </div>
-))
+storiesOf('colors', module)
+  .add('overview', () => (
+    <div style={{ margin: 40 }}>
+      <ColorExamples />
+    </div>
+  ))
+  .add('mapping v3 to v4 colors', () => <ColorMapping />)
