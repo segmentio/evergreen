ce66ed81f763b75e102c4b86723935e5815ad5fa
Flaque <econrad@zagmail.gonzaga.edu>
2018-08-07 14:38:34 -0700

    Add helpers, shared, and getS*.js

---
+++ src/theme/src/create-theme/base/getSegmentedControlRadioClassName.js
@@ -0,0 +1,31 @@
+import { Themer } from '../../../../themer'
+import memoizeClassName from '../utils/memoizeClassName'
+import { getDefaultControlStyles } from '../shared'
+
+export default styles => {
+  const defaultControlStyles = getDefaultControlStyles(styles)
+
+  const defaultAppearance = Themer.createSegmentedControlRadioAppearance({
+    base: defaultControlStyles.base,
+    disabled: defaultControlStyles.disabled,
+    hover: defaultControlStyles.hover,
+    active: defaultControlStyles.active,
+    focus: defaultControlStyles.focus
+  })
+
+  /**
+   * Get the appearanece of a `SegmentedControlRadio`.
+   * @param {string} appearance
+   * @return {string} the appearance object.
+   */
+  const getSegmentedControlRadioAppearance = () => {
+    return defaultAppearance
+  }
+
+  /**
+   * Get the className of a `SegmentedControlRadio`.
+   * @param {string} appearance
+   * @return {string} the appearance class name.
+   */
+  return memoizeClassName(getSegmentedControlRadioAppearance)
+}
