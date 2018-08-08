0972b54b158a769f32c0778c0d2235115c521fb9
Flaque <econrad@zagmail.gonzaga.edu>
2018-08-08 16:47:44 -0700

    Support server side rendering of url info

--- docs/src/pages/theme-builder.js
+++ docs/src/pages/theme-builder.js
@@ -1,14 +1,20 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import ThemeBuilder from '../theme-builder/'
 import TopBar from '../components/TopBar'
 import Layout from '../components/Layout'
 
 export default class Root extends React.Component {
+  static propTypes = {
+    location: PropTypes.object.isRequired
+  }
+
   componentDidCatch(error, errorInfo) {
     console.error(error, errorInfo)
   }
 
   render() {
+    const { location } = this.props
     return (
       <Layout>
         <div
@@ -21,7 +27,7 @@ export default class Root extends React.Component {
           }}
         >
           <TopBar />
-          <ThemeBuilder />
+          <ThemeBuilder location={location} />
         </div>
       </Layout>
     )
