f68626de929665c37fab9a31f3a151ce4b7f176f
jeroenransijn <jssrdesign@gmail.com>
2018-08-08 15:12:15 -0700

    fix color generation

--- src/theme/src/create-theme/createStyles.js
+++ src/theme/src/create-theme/createStyles.js
@@ -23,7 +23,6 @@ import themedProperty from './utils/themedProperty'
 
 function generateScale(color, prefix = 'P') {
   const scale = {}
-  const tinyPrimaryColor = tinycolor(color)
 
   const opacities = [0.04, 0.06, 0.09, 0.14, 0.3, 0.47, 0.7, 0.81]
 
@@ -36,11 +35,15 @@ function generateScale(color, prefix = 'P') {
   }
 
   scale[`${prefix}9`] = color
-  scale[`${prefix}10`] = tinyPrimaryColor.darken(15).toHexString()
+  scale[`${prefix}10`] = tinycolor(color)
+    .darken(15)
+    .toHexString()
 
   // Add opacity colors second.
   for (const [index, opacity] of opacities.entries()) {
-    const opacityColor = tinyPrimaryColor.setAlpha(opacity).toString()
+    const opacityColor = tinycolor(color)
+      .setAlpha(opacity)
+      .toString()
     scale[`${prefix}${index + 1}A`] = opacityColor
   }
 
