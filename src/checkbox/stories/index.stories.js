8362838524d82e77c40491d152b157d22209f18c
Jason Tu <jasontu4@gmail.com>
2018-08-31 15:08:29 -0700

    Clean up story

--- src/checkbox/stories/index.stories.js
+++ src/checkbox/stories/index.stories.js
@@ -3,26 +3,6 @@ import React from 'react'
 import Box from 'ui-box'
 import { Checkbox } from '../../checkbox'
 
-class CheckboxWithState extends React.Component {
-  state = {
-    checked: false
-  }
-
-  handleChange = () => {
-    this.setState({ checked: !this.state.checked })
-  }
-
-  render() {
-    return (
-      <Checkbox
-        checked={this.state.checked}
-        label="Checkbox"
-        onChange={this.handleChange}
-      />
-    )
-  }
-}
-
 storiesOf('checkbox', module).add('Checkbox', () => (
   <Box padding={40}>
     <Checkbox label="Checkbox default" />
@@ -31,6 +11,5 @@ storiesOf('checkbox', module).add('Checkbox', () => (
     <Checkbox disabled checked label="Checkbox checked disabled" />
     <Checkbox indeterminate label="Checkbox indeterminate" />
     <Checkbox checked indeterminate label="Checkbox checked indeterminate" />
-    <CheckboxWithState />
   </Box>
 ))
