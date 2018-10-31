e14704486aa8991d70debd7a12181449bb4ef037
kale-io <cos+a@iuvo.ai>
2018-10-31 23:50:54 +0000

    Revert: Enforce explicit boolean attributes notation in JSX Convert JSX attributes without explicit values (e.g., `<App att>`) to their explicit version (e.g., `<App att={true} />`)

--- src/checkbox/stories/index.stories.js
+++ src/checkbox/stories/index.stories.js
@@ -6,7 +6,7 @@ import { Checkbox } from '..'
 storiesOf('checkbox', module).add('Checkbox', () => (
   <Box padding={40}>
     <Checkbox label="Checkbox default" />
-    <Checkbox checked={true} label="Checkbox checked" />
+    <Checkbox checked label="Checkbox checked" />
     <Checkbox disabled label="Checkbox disabled" />
     <Checkbox disabled checked label="Checkbox checked disabled" />
     <Checkbox indeterminate label="Checkbox indeterminate" />
