22d6b9eeedeea20c2e1b5978bc84fb4fdd474e3e
Vadim Demedes <vdemedes@gmail.com>
2018-11-01 11:36:32 -0700

    Fix rendering of nodes via Portal (#413)

--- src/portal/src/Portal.js
+++ src/portal/src/Portal.js
@@ -19,9 +19,6 @@ export default class Portal extends Component {
     }
 
     this.el = document.createElement('div')
-  }
-
-  componentDidMount() {
     portalContainer.appendChild(this.el)
   }
 
