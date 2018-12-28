2947c129d01de7eeddf8efdbbf7d371c097e71a0
Matt Shwery <mshwery@gmail.com>
2018-12-28 14:21:28 -0600

    prevent body scroll (optional) in overlay

--- src/overlay/stories/index.stories.js
+++ src/overlay/stories/index.stories.js
@@ -29,21 +29,41 @@ class OverlayManager extends PureComponent {
   }
 }
 
-storiesOf('overlay', module).add('Overlay', () => (
-  <Box padding={40}>
-    {(() => {
-      document.body.style.margin = '0'
-      document.body.style.height = '100vh'
-    })()}
-    <OverlayManager>
-      {({ hide, show, isShown }) => (
-        <Box>
-          <Overlay isShown={isShown} onExited={hide}>
-            Overlay children
-          </Overlay>
-          <Button onClick={show}>Show Overlay</Button>
-        </Box>
-      )}
-    </OverlayManager>
-  </Box>
-))
+storiesOf('overlay', module)
+  .add('Overlay', () => (
+    <Box padding={40}>
+      {(() => {
+        document.body.style.margin = '0'
+        document.body.style.height = '100vh'
+      })()}
+      <OverlayManager>
+        {({ hide, show, isShown }) => (
+          <Box>
+            <Overlay isShown={isShown} onExited={hide}>
+              Overlay children
+            </Overlay>
+            <Button onClick={show}>Show Overlay</Button>
+          </Box>
+        )}
+      </OverlayManager>
+    </Box>
+  ))
+  .add('Prevent Body Scroll', () => (
+    <Box padding={40} paddingTop="50vh">
+      {(() => {
+        document.body.style.margin = '0'
+        document.body.style.height = '200vh'
+        document.body.style.background = 'gray'
+      })()}
+      <OverlayManager>
+        {({ hide, show, isShown }) => (
+          <Box>
+            <Overlay isShown={isShown} onExited={hide} preventBodyScrolling>
+              Overlay children
+            </Overlay>
+            <Button onClick={show}>Show Overlay</Button>
+          </Box>
+        )}
+      </OverlayManager>
+    </Box>
+  ))
