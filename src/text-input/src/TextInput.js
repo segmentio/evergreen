0849d9c970dab9408327bdc7e6d2c91a9672ca7b
Evan Conrad <econrad@zagmail.gonzaga.edu>
2019-02-21 11:13:12 -0800

    Add a better explanation of the validation message (#503)
    * Add a better explanation of the validation message
    
    * "unaffected"
    
    * Update SelectField.js
    
    * Update TextInputField.js

--- src/text-input/src/TextInput.js
+++ src/text-input/src/TextInput.js
@@ -22,7 +22,8 @@ class TextInput extends PureComponent {
     disabled: PropTypes.bool,
 
     /**
-     * Sets visual styling to be invalid.
+     * Sets visual styling of _only_ the text input to be "invalid". 
+     * Note that this does not effect any `validationMessage`.
      */
     isInvalid: PropTypes.bool,
 
