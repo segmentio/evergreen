fe2535ea36c03aa777d6b60e0adfeb9f76dd9b37
jongsue hong <jongsue777@gmail.com>
2019-02-26 08:12:26 +0900

    add highlightedIndex to destructure

--- src/autocomplete/src/Autocomplete.js
+++ src/autocomplete/src/Autocomplete.js
@@ -198,6 +198,7 @@ export default class Autocomplete extends PureComponent {
           getItemProps,
           selectedItem,
           selectItemAtIndex,
+          highlightedIndex,
           getRootProps,
           ...restDownshiftProps
         }) => (
