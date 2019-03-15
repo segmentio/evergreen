64c3327e9479bfb7cf36c5c7220f833acf346aa4
Gwenaël Galon <gwenael.gallon@mac.com>
2019-03-15 02:23:16 +0100

    Update @storybook/react ^3.4.11 to 5.0.1

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,3 +1,1 @@
-import '@storybook/addon-options/register'
-import '@storybook/addon-actions/register'
-import '@storybook/addon-links/register'
+import '@storybook/addon-knobs/register'
--- .storybook/config.js
+++ .storybook/config.js
@@ -1,71 +1,68 @@
-import { configure } from '@storybook/react'
-import { setOptions } from '@storybook/addon-options'
-import 'react-chromatic/storybook-addon'
+import { addParameters, configure } from '@storybook/react'
+import 'storybook-chromatic'
+import EvergreenTheme from './theme'
 
 // Option defaults:
-setOptions({
-  /**
-   * Name to display in the top left corner
-   * @type {String}
-   */
-  name: '🌲 Evergreen',
-  /**
-   * URL for name in top left corner to link to
-   * @type {String}
-   */
-  url: 'https://github.com/segmentio/evergreen',
-  /**
-   * Show story component as full screen
-   * @type {Boolean}
-   */
-  goFullScreen: false,
-  /**
-   * Display left panel that shows a list of stories
-   * @type {Boolean}
-   */
-  showStoriesPanel: true,
-  /**
-   * Display horizontal panel that displays addon configurations
-   * @type {Boolean}
-   */
-  showAddonPanel: false,
-  /**
-   * Display floating search box to search through stories
-   * @type {Boolean}
-   */
-  showSearchBox: false,
-  /**
-   * Show horizontal addons panel as a vertical panel on the right
-   * @type {Boolean}
-   */
-  addonPanelInRight: false,
-  /**
-   * Sorts stories
-   * @type {Boolean}
-   */
-  sortStoriesByKind: false,
-  /**
-   * Regex for finding the hierarchy separator
-   * @example:
-   *   null - turn off hierarchy
-   *   /\// - split by `/`
-   *   /\./ - split by `.`
-   *   /\/|\./ - split by `/` or `.`
-   * @type {Regex}
-   */
-  hierarchySeparator: null,
-
-  /**
-   * Sidebar tree animations
-   * @type {Boolean}
-   */
-  sidebarAnimations: true,
-
-  /**
-   * ID to select an addon panel
-   * @type {String}
-   */
-  selectedAddonPanel: undefined // The order of addons in the "Addons Panel" is the same as you import them in 'addons.js'. The first panel will be opened by default as you run Storybook
+addParameters({
+  options: {
+    /**
+     * show story component as full screen
+     * @type {Boolean}
+     */
+    isFullScreen: false,
+    /**
+     * display panel that shows a list of stories
+     * @type {Boolean}
+     */
+    showNav: true,
+    /**
+     * display panel that shows addon configurations
+     * @type {Boolean}
+     */
+    showPanel: false,
+    /**
+     * where to show the addon panel
+     * @type {String}
+     */
+    panelPosition: 'bottom',
+    /**
+     * sorts stories
+     * @type {Boolean}
+     */
+    sortStoriesByKind: false,
+    /**
+     * regex for finding the hierarchy separator
+     * @example:
+     *   null - turn off hierarchy
+     *   /\// - split by `/`
+     *   /\./ - split by `.`
+     *   /\/|\./ - split by `/` or `.`
+     * @type {Regex}
+     */
+    hierarchySeparator: /\/|\./,
+    /**
+     * regex for finding the hierarchy root separator
+     * @example:
+     *   null - turn off multiple hierarchy roots
+     *   /\|/ - split by `|`
+     * @type {Regex}
+     */
+    hierarchyRootSeparator: /\|/,
+    /**
+     * sidebar tree animations
+     * @type {Boolean}
+     */
+    sidebarAnimations: true,
+    /**
+     * enable/disable shortcuts
+     * @type {Boolean}
+     */
+    enableShortcuts: true,
+    /**
+     * theme storybook, see link below
+     */
+    theme: EvergreenTheme
+  }
 })
 
 function loadStories() {
