3f49f88af8bf92c75fa3a65c83d6bde816765c63
Jeroen Peeters <contact@jeroenpeeters.me>
2019-04-07 19:26:31 +0200

    chore: storybook typescript setup

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const SRC_PATH = path.join(__dirname, '../src')
 const webpack = require('webpack')
 
 module.exports = storybookBaseConfig => {
@@ -26,8 +27,24 @@ module.exports = storybookBaseConfig => {
         {
           test: /\.(json)/,
           loader: 'json-loader'
+        },
+        {
+          test: /\.(ts|tsx)$/,
+          include: [SRC_PATH],
+          use: [
+            {
+              loader: require.resolve('awesome-typescript-loader'),
+              options: {
+                configFileName: './.storybook/tsconfig.json'
+              }
+            },
+            { loader: require.resolve('react-docgen-typescript-loader') }
+          ]
         }
       ]
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx']
     }
   }
 }
