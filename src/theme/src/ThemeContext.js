fef00132afff414a501b58235486b9d358fef734
Fede Ramirez <i@2fd.me>
2019-06-16 20:27:47 -0300

    expose the`ThemeContext` to use the new context hook (#578)

--- src/theme/src/ThemeContext.js
+++ src/theme/src/ThemeContext.js
@@ -4,9 +4,11 @@ import defaultTheme from './default-theme'
 /**
  * Use React 16.3+ createContext API.
  */
+const ThemeContext = React.createContext(defaultTheme)
 const {
   Provider: ThemeProvider,
   Consumer: ThemeConsumer
-} = React.createContext(defaultTheme)
+} = ThemeContext
 
+export default ThemeContext
 export { ThemeProvider, ThemeConsumer }
