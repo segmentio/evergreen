d4d1db565b182a9c8f4fcf69ca566c37c6a9130f
Rob Phoenix <rob@robphoenix.com>
2019-07-17 13:49:18 +0100

    Add types to minorScale

--- src/scales/src/minorScale.tsx
+++ src/scales/src/minorScale.tsx
@@ -1,12 +1,12 @@
 /**
  * Helper function to conform to 4 (px) minor scale.
- * @param {number} x — unitless number
+ * @param {number} x - unitless number
  * @return {number} - unitless number
  * @example
  * minorScale(3) => 3*4 = 12
  * minorScale(1) => 1*4 = 4
  */
-export default function minorScale(x) {
+export default function minorScale(x: number): number {
   if (!Number.isInteger(x)) {
     throw new TypeError(
       `minorScale only accepts integers as input, instead ${x} was passed.`
