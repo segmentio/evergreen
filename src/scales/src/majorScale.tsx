3d57038d68966b0c5d213f2c6f93562a67494e81
Rob Phoenix <rob@robphoenix.com>
2019-07-17 13:46:20 +0100

    Add types to majorScale

--- src/scales/src/majorScale.tsx
+++ src/scales/src/majorScale.tsx
@@ -1,12 +1,12 @@
 /**
  * Helper function to conform to 8 (px) major scale.
- * @param {number} x — unitless number
+ * @param {number} x - unitless number
  * @return {number} - unitless number
  * @example
  * majorScale(2) => 2*8 = 16
  * majorScale(4) => 4*8 = 32
  */
-export default function majorScale(x) {
+export default function majorScale(x: number): number {
   if (!Number.isInteger(x)) {
     throw new TypeError(
       `majorScale only accepts integers as input, instead ${x} was passed.`
