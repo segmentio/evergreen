9d1c1abc4f8f415017e0ec4e302df8e144373364
Pasalietis <pasalietis.sku@gmail.com>
2019-10-25 15:44:14 +0300

    Adding specific module load path

--- docs/src/components/DocsMDXProvider.js
+++ docs/src/components/DocsMDXProvider.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { MDXProvider } from '@mdx-js/tag'
+import { MDXProvider } from '../../node_modules/@mdx-js/react'
 import MDXPlayground from './MDXPlayground'
 
 /**
