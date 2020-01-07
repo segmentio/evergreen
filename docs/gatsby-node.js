9990190c464216d8141aabab4a27aad8ac0f231f
Chris Chuck <chrischuck@Chriss-MacBook-Pro.local>
2020-01-06 19:11:39 -0800

    Makes docs app use same react instance as evergreen-ui

--- docs/gatsby-node.js
+++ docs/gatsby-node.js
@@ -23,6 +23,7 @@ exports.onCreateWebpackConfig = ({ actions }) => {
       // Force Gatsby to look for dependencies within the local node_modules from docs.
       modules: ['node_modules'],
       alias: {
+        react: path.resolve(__dirname, '../node_modules/react'),
         'evergreen-ui': path.resolve(__dirname, '../src/index.js'),
         components: path.resolve(__dirname, './src/components')
       }
