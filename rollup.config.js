3d8d95d80f73965e0f2ba6493e4eb00be1d4e1db
Yue Yang <g1enyy0ung@gmail.com>
2020-01-23 00:13:41 +0800

    Add UMD support

---
+++ rollup.config.js
@@ -0,0 +1,85 @@
+import babel from 'rollup-plugin-babel'
+import commonjs from '@rollup/plugin-commonjs'
+import replace from '@rollup/plugin-replace'
+import resolve from '@rollup/plugin-node-resolve'
+import { terser } from 'rollup-plugin-terser'
+import pkg from './package.json'
+
+const { peerDependencies } = pkg
+const external = Object.keys(peerDependencies)
+
+const namedExports = {
+  'node_modules/downshift/node_modules/react-is/index.js': ['isForwardRef'],
+  'node_modules/react-tiny-virtual-list/node_modules/prop-types/index.js': [
+    'array',
+    'arrayOf',
+    'func',
+    'number',
+    'object',
+    'oneOf',
+    'oneOfType',
+    'string'
+  ]
+}
+
+export default [
+  // UMD Development
+  {
+    input: 'src/index.umd.js',
+    external,
+    output: {
+      file: 'umd/evergreen.js',
+      format: 'umd',
+      name: 'EvergreenUI',
+      indent: false,
+      globals: {
+        react: 'React',
+        'react-dom': 'ReactDOM'
+      }
+    },
+    plugins: [
+      resolve(),
+      commonjs({
+        include: 'node_modules/**',
+        namedExports
+      }),
+      replace({
+        'process.env.NODE_ENV': JSON.stringify('development')
+      }),
+      babel({
+        exclude: 'node_modules/**',
+        runtimeHelpers: true
+      })
+    ]
+  },
+  // UMD Production
+  {
+    input: 'src/index.umd.js',
+    external,
+    output: {
+      file: 'umd/evergreen.min.js',
+      format: 'umd',
+      name: 'EvergreenUI',
+      indent: false,
+      globals: {
+        react: 'React',
+        'react-dom': 'ReactDOM'
+      }
+    },
+    plugins: [
+      resolve(),
+      commonjs({
+        include: 'node_modules/**',
+        namedExports
+      }),
+      replace({
+        'process.env.NODE_ENV': JSON.stringify('production')
+      }),
+      babel({
+        exclude: 'node_modules/**',
+        runtimeHelpers: true
+      }),
+      terser()
+    ]
+  }
+]
---
+++ src/index.umd.js
@@ -0,0 +1,84 @@
+export { Alert, InlineAlert } from './alert'
+export { Autocomplete, AutocompleteItem } from './autocomplete'
+export { Avatar } from './avatar'
+export { Badge, Pill } from './badges'
+export { BackButton, Button, IconButton, TextDropdownButton } from './buttons'
+export { Checkbox } from './checkbox'
+export { Combobox } from './combobox'
+export { StackingOrder, Intent, Position } from './constants'
+export { CornerDialog } from './corner-dialog'
+export { Dialog } from './dialog'
+export { FilePicker } from './file-picker'
+export {
+  FormField,
+  FormFieldDescription,
+  FormFieldHint,
+  FormFieldLabel,
+  FormFieldValidationMessage
+} from './form-field'
+export { Icon, IconNames } from './icon'
+export { Image } from './image'
+export { Pane, Card } from './layers'
+export { Menu } from './menu'
+export { Overlay } from './overlay'
+export { Popover } from './popover'
+export { Portal } from './portal'
+export { Positioner } from './positioner'
+export { Radio, RadioGroup } from './radio'
+export { minorScale, majorScale } from './scales'
+export { SearchInput } from './search-input'
+export { SegmentedControl } from './segmented-control'
+export { Select, SelectField } from './select'
+export {
+  OptionShapePropType,
+  OptionsList,
+  SelectedPropType,
+  SelectMenu,
+  SelectMenuContent
+} from './select-menu'
+export { SideSheet } from './side-sheet'
+export { Spinner } from './spinner'
+export { Switch } from './switch'
+export { Stack, StackingContext } from './stack'
+export {
+  Table,
+  TableHead,
+  TableHeaderCell,
+  TextTableHeaderCell,
+  SearchTableHeaderCell,
+  TableBody,
+  TableRow,
+  TableCell,
+  TextTableCell
+} from './table'
+export { SidebarTab, Tab, Tablist, TabNavigation } from './tabs'
+export { TagInput } from './tag-input'
+export { TextInput, TextInputField } from './text-input'
+export {
+  ThemeContext,
+  ThemeProvider,
+  ThemeConsumer,
+  withTheme,
+  useTheme,
+  defaultTheme
+} from './theme'
+export { Textarea } from './textarea'
+export { toaster } from './toaster'
+export { Tooltip } from './tooltip'
+export {
+  UnorderedList,
+  Ul,
+  OrderedList,
+  Ol,
+  ListItem,
+  Li,
+  Text,
+  Paragraph,
+  Heading,
+  Code,
+  Pre,
+  Label,
+  Link,
+  Small,
+  Strong
+} from './typography'
