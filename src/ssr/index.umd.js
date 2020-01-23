326631a1ff910cbe8dad8b2f24d37e42f61b5b09
Yue Yang <g1enyy0ung@gmail.com>
2020-01-24 00:01:34 +0800

    chore: add warning in ssr umd exports

--- src/ssr/index.umd.js
+++ src/ssr/index.umd.js
@@ -1,2 +1,13 @@
-export function extractStyles() {}
-export function autoHydrate() {}
+export function extractStyles() {
+  console.warn(
+    'extractStyles returns null in the UMD bundle. Please use cjs or esm when you develop an SSR app.'
+  )
+
+  return null
+}
+
+export function autoHydrate() {
+  console.warn(
+    'autoHydrate is an empty function in the UMD bundle. Please use cjs or esm when you develop an SSR app.'
+  )
+}
