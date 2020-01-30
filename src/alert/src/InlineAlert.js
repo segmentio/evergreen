d189e145e5a7630c7a968640f7ade9c460b59136
Chris Chuck <chrischuck@Chriss-MacBook-Pro.local>
2020-01-29 22:26:14 -0800

    Migrates alert component to hooks

--- src/alert/src/InlineAlert.js
+++ src/alert/src/InlineAlert.js
@@ -1,70 +1,64 @@
-import React, { PureComponent } from 'react'
+import React, { memo, forwardRef } from 'react'
 import PropTypes from 'prop-types'
 import { spacing, dimensions, position, layout } from 'ui-box'
-import { withTheme } from '../../theme'
+import { useTheme } from '../../theme'
 import { Pane } from '../../layers'
 import { Text } from '../../typography'
 
-class InlineAlert extends PureComponent {
-  static propTypes = {
-    /**
-     * Composes some Box APIs.
-     */
-    ...spacing.propTypes,
-    ...position.propTypes,
-    ...layout.propTypes,
-    ...dimensions.propTypes,
+function InlineAlert(
+  { children, intent = 'none', hasIcon = true, size = 400, ...props },
+  ref
+) {
+  const theme = useTheme()
 
-    /**
-     * The content of the alert.
-     */
-    children: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
-
-    /**
-     * The intent of the alert. This should always be set explicitly.
-     */
-    intent: PropTypes.oneOf(['none', 'success', 'warning', 'danger'])
-      .isRequired,
-
-    /**
-     * When true, show a icon on the left matching the type.
-     * There is no point not showing this.
-     */
-    hasIcon: PropTypes.bool,
-
-    /**
-     * The size of the Text.
-     */
-    size: PropTypes.number,
-
-    /**
-     * Theme provided by ThemeProvider.
-     */
-    theme: PropTypes.object.isRequired
-  }
-
-  static defaultProps = {
-    intent: 'none',
-    hasIcon: true,
-    size: 400
-  }
-
-  render() {
-    const { theme, children, intent, hasIcon, size, ...props } = this.props
-
-    return (
-      <Pane alignItems="center" display="flex" {...props}>
-        {hasIcon && (
-          <Pane display="inline" marginRight={8}>
-            {theme.getIconForIntent(intent, { size: 14, marginTop: 2 })}
-          </Pane>
-        )}
-        <Text size={size} fontWeight={500}>
-          {children}
-        </Text>
-      </Pane>
-    )
-  }
+  return (
+    <Pane alignItems="center" display="flex" innerRef={ref} {...props}>
+      {hasIcon && (
+        <Pane display="inline" marginRight={8}>
+          <Icon size={14} marginTop={2} {...theme.getIconForIntent(intent)} />
+        </Pane>
+      )}
+      <Text size={size} fontWeight={500}>
+        {children}
+      </Text>
+    </Pane>
+  )
 }
 
-export default withTheme(InlineAlert)
+InlineAlert.propTypes = {
+  /**
+   * Composes some Box APIs.
+   */
+  ...spacing.propTypes,
+  ...position.propTypes,
+  ...layout.propTypes,
+  ...dimensions.propTypes,
+
+  /**
+   * The content of the alert.
+   */
+  children: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+
+  /**
+   * The intent of the alert. This should always be set explicitly.
+   */
+  intent: PropTypes.oneOf(['none', 'success', 'warning', 'danger']).isRequired,
+
+  /**
+   * When true, show a icon on the left matching the type.
+   * There is no point not showing this.
+   */
+  hasIcon: PropTypes.bool,
+
+  /**
+   * The size of the Text.
+   */
+  size: PropTypes.number,
+
+  /**
+   * Theme provided by ThemeProvider.
+   */
+  theme: PropTypes.object.isRequired
+}
+
+export default memo(forwardRef(InlineAlert))
