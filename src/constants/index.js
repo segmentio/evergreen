20cf33c21596653bf156747099dc0516a087dcfc
hfa0 <husein.fakhir@bitmovin.com>
2020-03-17 21:00:02 +0100

    introduce position for toast

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,4 @@
 export { default as StackingOrder } from './src/StackingOrder'
 export { default as Intent } from './src/Intent'
 export { default as Position } from './src/Position'
+export { default as ToastPosition } from './src/ToastPosition'
---
+++ src/constants/src/ToastPosition.js
@@ -0,0 +1,8 @@
+export default {
+  BOTTOM: 'bottom',
+  BOTTOM_LEFT: 'bottom-left',
+  BOTTOM_RIGHT: 'bottom-right',
+  TOP: 'top',
+  TOP_LEFT: 'top-left',
+  TOP_RIGHT: 'top-right'
+}
