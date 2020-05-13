e9684b6e35cb5f6a7c6e109bb64a9acb869a76e7
Matt Shwery <matthew.shwery@segment.com>
2020-05-13 10:54:31 -0500

    remove v3->v4 color mapping (#821)

--- src/colors/stories/index.stories.js
+++ src/colors/stories/index.stories.js
@@ -1,12 +1,9 @@
 import { storiesOf } from '@storybook/react'
 import React from 'react'
 import ColorExamples from './ColorExamples'
-import ColorMapping from './ColorMapping'
 
-storiesOf('colors', module)
-  .add('overview', () => (
-    <div style={{ margin: 40 }}>
-      <ColorExamples />
-    </div>
-  ))
-  .add('mapping v3 to v4 colors', () => <ColorMapping />)
+storiesOf('colors', module).add('overview', () => (
+  <div style={{ margin: 40 }}>
+    <ColorExamples />
+  </div>
+))
