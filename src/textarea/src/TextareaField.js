6f93558e2ee18a9e3c833c63396d752f7aa30348
Allen Kleiner <akleiner24@gmail.com>
2020-05-14 08:38:42 -0700

    Bump <Textarea /> and <TextareaField /> to memo + forwardRef

--- src/textarea/src/TextareaField.js
+++ src/textarea/src/TextareaField.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react'
+import React, { memo, forwardRef, useState } from 'react'
 import PropTypes from 'prop-types'
 import { splitBoxProps } from 'ui-box'
 import { FormField } from '../../form-field'
@@ -6,64 +6,10 @@ import Textarea from './Textarea'
 
 let idCounter = 0
 
-export default class TextareaField extends PureComponent {
-  static propTypes = {
-    /**
-     * Composes the Textarea component as the base.
-     */
-    ...Textarea.propTypes,
-    ...FormField.propTypes,
+const TextareaField = memo(
+  forwardRef((props, ref) => {
+    const [id] = useState(props.id || `TextareaField-${idCounter++}`)
 
-    /**
-     * The label used above the input element.
-     */
-    label: PropTypes.node.isRequired,
-
-    /**
-     * Whether or not to show an asterix after the label.
-     */
-    required: PropTypes.bool,
-
-    /**
-     * An optional description of the field under the label, above the input element.
-     */
-    description: PropTypes.node,
-
-    /**
-     * An optional hint under the input element.
-     */
-    hint: PropTypes.node,
-
-    /**
-     * If a validation message is passed it is shown under the input element
-     * and above the hint. This is unaffected by `isInvalid`.
-     */
-    validationMessage: PropTypes.node,
-
-    /**
-     * The height of the input element.
-     */
-    inputHeight: PropTypes.number,
-
-    /**
-     * The width of the input width.
-     */
-    inputWidth: PropTypes.oneOfType([PropTypes.number, PropTypes.string])
-  }
-
-  static defaultProps = {
-    /**
-     * The input width should be as wide as the form field.
-     */
-    inputWidth: '100%',
-    inputHeight: 80
-  }
-
-  state = {
-    id: (this.props.id || idCounter++).toString()
-  }
-
-  render() {
     const {
       // We are using the id from the state
       id: unusedId,
@@ -75,20 +21,18 @@ export default class TextareaField extends PureComponent {
       validationMessage,
 
       // Textarea props
-      inputHeight,
-      inputWidth,
       disabled,
       required,
       isInvalid,
       appearance,
       placeholder,
       spellCheck,
+      inputHeight = 80,
+      inputWidth = '100%',
 
       // Rest props are spread on the FormField
-      ...props
-    } = this.props
-
-    const id = `TextareaField-${this.state.id}`
+      ...restProps
+    } = props
 
     /**
      * Split the wrapper props from the input props.
@@ -105,6 +49,7 @@ export default class TextareaField extends PureComponent {
         validationMessage={validationMessage}
         labelFor={id}
         {...matchedProps}
+        {...restProps}
       >
         <Textarea
           id={id}
@@ -116,9 +61,56 @@ export default class TextareaField extends PureComponent {
           appearance={appearance}
           placeholder={placeholder}
           spellCheck={spellCheck}
+          ref={ref}
           {...remainingProps}
         />
       </FormField>
     )
-  }
+  })
+)
+
+TextareaField.propTypes = {
+  /**
+   * Composes the Textarea component as the base.
+   */
+  ...Textarea.propTypes,
+  ...FormField.propTypes,
+
+  /**
+   * The label used above the input element.
+   */
+  label: PropTypes.node.isRequired,
+
+  /**
+   * Whether or not to show an asterix after the label.
+   */
+  required: PropTypes.bool,
+
+  /**
+   * An optional description of the field under the label, above the input element.
+   */
+  description: PropTypes.node,
+
+  /**
+   * An optional hint under the input element.
+   */
+  hint: PropTypes.node,
+
+  /**
+   * If a validation message is passed it is shown under the input element
+   * and above the hint. This is unaffected by `isInvalid`.
+   */
+  validationMessage: PropTypes.node,
+
+  /**
+   * The height of the input element.
+   */
+  inputHeight: PropTypes.number,
+
+  /**
+   * The width of the input width.
+   */
+  inputWidth: PropTypes.oneOfType([PropTypes.number, PropTypes.string])
 }
+
+export default TextareaField
