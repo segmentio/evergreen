cad106210d00d3ab3048bcac09cfb18a235589d0
Allen Kleiner <akleiner24@gmail.com>
2020-05-24 21:04:17 -0700

    update tablebody ref support

--- src/table/src/TableBody.js
+++ src/table/src/TableBody.js
@@ -1,20 +1,27 @@
-import React, { PureComponent } from 'react'
+import React, { memo, forwardRef } from 'react'
 import { Pane } from '../../layers'
 
-export default class TableBody extends PureComponent {
-  static propTypes = {
-    /**
-     * Composes the Pane component as the base.
-     */
-    ...Pane.propTypes
-  }
-
-  render() {
-    const { children, ...props } = this.props
+const TableBody = memo(
+  forwardRef(({ children, ...props }, ref) => {
     return (
-      <Pane data-evergreen-table-body flex="1" overflowY="auto" {...props}>
+      <Pane
+        data-evergreen-table-body
+        flex="1"
+        overflowY="auto"
+        ref={ref}
+        {...props}
+      >
         {children}
       </Pane>
     )
-  }
+  })
+)
+
+TableBody.propTypes = {
+  /**
+   * Composes the Pane component as the base.
+   */
+  ...Pane.propTypes
 }
+
+export default TableBody
