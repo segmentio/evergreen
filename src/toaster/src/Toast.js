64919ebfe8fc3ce64932bc310b37d363e596b11d
colinlohner <colin@colinlohner.com>
2020-07-15 01:59:18 -0700

    Update Toast to use hooks and be a FC

--- src/toaster/src/Toast.js
+++ src/toaster/src/Toast.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { memo, useState, useEffect } from 'react'
 import { css } from 'glamor'
 import PropTypes from 'prop-types'
 import { Transition } from 'react-transition-group'
@@ -47,154 +47,153 @@ const animationStyles = css({
   }
 })
 
-export default class Toast extends React.PureComponent {
-  static propTypes = {
-    /**
-     * The z-index of the toast.
-     */
-    zIndex: PropTypes.number,
+const Toast = memo((props) => {
+  const {
+    duration,
+    onRemove,
+    isShown: isShownProp,
+    // Template props
+    intent = 'none',
+    zIndex,
+    title,
+    children,
+    hasCloseButton
+  } = props
 
-    /**
-     * Duration of the toast.
-     */
-    duration: PropTypes.number,
+  const [isShown, setIsShown] = useState(true)
+  const [closeTimer, setCloseTimer] = useState(null)
+  const [height, setHeight] = useState(0)
 
-    /**
-     * Function called when the toast is all the way closed.
-     */
-    onRemove: PropTypes.func,
-
-    /**
-     * The type of the alert.
-     */
-    intent: PropTypes.oneOf(['none', 'success', 'warning', 'danger'])
-      .isRequired,
-
-    /**
-     * The title of the alert.
-     */
-    title: PropTypes.node,
-
-    /**
-     * Description of the alert.
-     */
-    children: PropTypes.node,
-
-    /**
-     * When true, show a close icon button inside of the toast.
-     */
-    hasCloseButton: PropTypes.bool,
-
-    /**
-     * When false, will close the Toast and call onRemove when finished.
-     */
-    isShown: PropTypes.bool
+  const close = () => {
+    clearCloseTimer()
+    setIsShown(false)
   }
 
-  static defaultProps = {
-    intent: 'none'
-  }
-
-  state = {
-    isShown: true,
-    height: 0
-  }
-
-  componentDidUpdate(prevProps) {
-    if (prevProps.isShown !== this.props.isShown) {
-      // eslint-disable-next-line react/no-did-update-set-state
-      this.setState({
-        isShown: this.props.isShown
-      })
+  const startCloseTimer = () => {
+    if (duration) {
+      setCloseTimer(setTimeout(() => {
+        close()
+      }, duration * 1000))
     }
   }
 
-  componentDidMount() {
-    this.startCloseTimer()
-  }
-
-  componentWillUnmount() {
-    this.clearCloseTimer()
-  }
-
-  close = () => {
-    this.clearCloseTimer()
-    this.setState({
-      isShown: false
-    })
-  }
-
-  startCloseTimer = () => {
-    if (this.props.duration) {
-      this.closeTimer = setTimeout(() => {
-        this.close()
-      }, this.props.duration * 1000)
+  const clearCloseTimer = () => {
+    if (closeTimer) {
+      clearTimeout(closeTimer)
+      setCloseTimer(null)
     }
   }
 
-  clearCloseTimer = () => {
-    if (this.closeTimer) {
-      clearTimeout(this.closeTimer)
-      this.closeTimer = null
+  useEffect(() => {
+    startCloseTimer()
+
+    return () => {
+      clearCloseTimer()
     }
+  }, [])
+
+  useEffect(() => {
+    if (isShownProp !== isShown && typeof isShownProp === 'boolean') {
+      setIsShown(isShownProp)
+    }
+  }, [isShownProp])
+
+  const handleMouseEnter = () => {
+    clearCloseTimer()
   }
 
-  handleMouseEnter = () => {
-    this.clearCloseTimer()
+  const handleMouseLeave = () => {
+    startCloseTimer()
   }
 
-  handleMouseLeave = () => {
-    this.startCloseTimer()
-  }
-
-  onRef = ref => {
+  const onRef = (ref) => {
     if (ref === null) return
 
-    const { height } = ref.getBoundingClientRect()
-
-    this.setState({
-      height
-    })
+    const { height: rectHeight } = ref.getBoundingClientRect()
+    setHeight(rectHeight)
   }
-
-  render() {
-    return (
-      <Transition
-        appear
-        unmountOnExit
-        timeout={ANIMATION_DURATION}
-        in={this.state.isShown}
-        onExited={this.props.onRemove}
-      >
-        {state => (
-          <div
-            data-state={state}
-            className={animationStyles}
-            onMouseEnter={this.handleMouseEnter}
-            onMouseLeave={this.handleMouseLeave}
-            style={{
-              height: this.state.height,
-              zIndex: this.props.zIndex,
-              marginBottom: this.state.isShown ? 0 : -this.state.height
-            }}
-          >
-            <div ref={this.onRef} style={{ padding: 8 }}>
-              <Alert
-                flexShrink={0}
-                appearance="card"
-                elevation={3}
-                intent={this.props.intent}
-                title={this.props.title}
-                isRemoveable={this.props.hasCloseButton}
-                onRemove={() => this.close()}
-                pointerEvents="all"
-              >
-                {this.props.children}
-              </Alert>
-            </div>
+  
+  return (
+    <Transition
+      appear
+      unmountOnExit
+      timeout={ANIMATION_DURATION}
+      in={isShown}
+      onExited={onRemove}
+    >
+      {state => (
+        <div
+          data-state={state}
+          className={animationStyles}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+          style={{
+            height: height,
+            zIndex: zIndex,
+            marginBottom: isShown ? 0 : -height
+          }}
+        >
+          <div ref={onRef} style={{ padding: 8 }}>
+            <Alert
+              flexShrink={0}
+              appearance="card"
+              elevation={3}
+              intent={intent}
+              title={title}
+              isRemoveable={hasCloseButton}
+              onRemove={() => close()}
+              pointerEvents="all"
+            >
+              {children}
+            </Alert>
           </div>
-        )}
-      </Transition>
-    )
-  }
+        </div>
+      )}
+    </Transition>
+  )
+})
+
+Toast.propTypes = {
+  /**
+   * The z-index of the toast.
+   */
+  zIndex: PropTypes.number,
+
+  /**
+   * Duration of the toast.
+   */
+  duration: PropTypes.number,
+
+  /**
+   * Function called when the toast is all the way closed.
+   */
+  onRemove: PropTypes.func,
+
+  /**
+   * The type of the alert.
+   */
+  intent: PropTypes.oneOf(['none', 'success', 'warning', 'danger'])
+    .isRequired,
+
+  /**
+   * The title of the alert.
+   */
+  title: PropTypes.node,
+
+  /**
+   * Description of the alert.
+   */
+  children: PropTypes.node,
+
+  /**
+   * When true, show a close icon button inside of the toast.
+   */
+  hasCloseButton: PropTypes.bool,
+
+  /**
+   * When false, will close the Toast and call onRemove when finished.
+   */
+  isShown: PropTypes.bool
 }
+
+export default Toast
