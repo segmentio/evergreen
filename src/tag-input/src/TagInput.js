900b382f6c0ef1404eea735a4c2048d0232395d7
Matt Shwery <mshwery@gmail.com>
2020-07-28 14:24:08 -0500

    support forwardRef on TagInput container

--- src/tag-input/src/TagInput.js
+++ src/tag-input/src/TagInput.js
@@ -2,7 +2,7 @@
  * @overview TagInput accepts multiple values that can be individually removed
  */
 
-import React, { memo, useState } from 'react'
+import React, { memo, forwardRef, useState } from 'react'
 import PropTypes from 'prop-types'
 import Box from 'ui-box'
 import cx from 'classnames'
@@ -18,171 +18,175 @@ const GET_KEY_FOR_TAG_DELIMITER = {
   space: ' '
 }
 
-const TagInput = memo(function TagInput(props) {
-  const {
-    addOnBlur = false,
-    disabled = false,
-    height = 32,
-    separator = /[,\n\r]/,
-    values = [],
-    tagSubmitKey = 'enter',
-    tagProps = {},
-    onAdd,
-    onChange,
-    onRemove,
-    onBlur,
-    onFocus,
-    onInputChange,
-    className,
-    inputProps,
-    inputRef,
-    ...rest
-  } = props
-  const theme = useTheme()
+const TagInput = memo(
+  forwardRef(function TagInput(props, ref) {
+    const {
+      addOnBlur = false,
+      disabled = false,
+      height = 32,
+      separator = /[,\n\r]/,
+      values = [],
+      tagSubmitKey = 'enter',
+      tagProps = {},
+      onAdd,
+      onChange,
+      onRemove,
+      onBlur,
+      onFocus,
+      onInputChange,
+      className,
+      inputProps,
+      inputRef,
+      ...rest
+    } = props
+    const theme = useTheme()
 
-  const [inputValue, setInputValue] = useState('')
-  const [isFocused, setIsFocused] = useState(false)
-  const id = useId('TagInput')
+    const [inputValue, setInputValue] = useState('')
+    const [isFocused, setIsFocused] = useState(false)
+    const id = useId('TagInput')
 
-  const getValues = (inputValue = '') =>
-    separator
-      ? inputValue
-          .split(separator)
-          .map(v => v.trim())
-          .filter(v => v.length > 0)
-      : [inputValue]
+    const getValues = (inputValue = '') =>
+      separator
+        ? inputValue
+            .split(separator)
+            .map(v => v.trim())
+            .filter(v => v.length > 0)
+        : [inputValue]
 
-  const addTags = (value = '') => {
-    const newValues = getValues(value)
-    let shouldClearInput = safeInvoke(onAdd, newValues)
+    const addTags = (value = '') => {
+      const newValues = getValues(value)
+      let shouldClearInput = safeInvoke(onAdd, newValues)
 
-    if (typeof onChange === 'function') {
-      shouldClearInput = shouldClearInput || onChange(values.concat(newValues))
-    }
-
-    if (shouldClearInput !== false) {
-      setInputValue('')
-    }
-  }
-
-  const removeTagAtIndex = index => {
-    safeInvoke(onRemove, values[index], index)
-
-    // Remove item at index as a new array
-    const newValues = values.filter((_, i) => i !== index)
-    safeInvoke(onChange, newValues)
-  }
-
-  const handleBackspaceToRemove = () => {
-    removeTagAtIndex(values.length - 1)
-  }
-
-  const handleBlur = event => {
-    const container = event.target
-
-    requestAnimationFrame(() => {
-      if (!container.contains(document.activeElement)) {
-        if (addOnBlur && inputValue) {
-          addTags(inputValue)
-        }
-
-        setIsFocused(false)
+      if (typeof onChange === 'function') {
+        shouldClearInput =
+          shouldClearInput || onChange(values.concat(newValues))
       }
-    })
 
-    safeInvoke(onBlur, event)
-  }
-
-  const handleInputChange = event => {
-    setInputValue(event.target.value)
-    safeInvoke(onInputChange, event)
-  }
-
-  const handleInputFocus = event => {
-    setIsFocused(true)
-    safeInvoke(onFocus, event)
-  }
-
-  const handleKeyDown = event => {
-    const { selectionEnd, value } = event.target
-    const key = GET_KEY_FOR_TAG_DELIMITER[tagSubmitKey]
-
-    if (event.key === key) {
-      event.preventDefault()
-      addTags(value)
-    } else if (event.key === 'Backspace' && selectionEnd === 0) {
-      handleBackspaceToRemove(event)
-    }
-  }
-
-  const handleRemoveTag = event => {
-    // Using data attribute to simplify callback logic -- one handler for all children
-    const index = Number(
-      event.currentTarget.parentElement.getAttribute('data-tag-index')
-    )
-    removeTagAtIndex(index)
-  }
-
-  const maybeRenderTag = (tag, index) => {
-    if (!tag) {
-      return null
+      if (shouldClearInput !== false) {
+        setInputValue('')
+      }
     }
 
-    const propsForElement = safeInvoke(tagProps, tag, index) || tagProps
+    const removeTagAtIndex = index => {
+      safeInvoke(onRemove, values[index], index)
+
+      // Remove item at index as a new array
+      const newValues = values.filter((_, i) => i !== index)
+      safeInvoke(onChange, newValues)
+    }
+
+    const handleBackspaceToRemove = () => {
+      removeTagAtIndex(values.length - 1)
+    }
+
+    const handleBlur = event => {
+      const container = event.target
+
+      requestAnimationFrame(() => {
+        if (!container.contains(document.activeElement)) {
+          if (addOnBlur && inputValue) {
+            addTags(inputValue)
+          }
+
+          setIsFocused(false)
+        }
+      })
+
+      safeInvoke(onBlur, event)
+    }
+
+    const handleInputChange = event => {
+      setInputValue(event.target.value)
+      safeInvoke(onInputChange, event)
+    }
+
+    const handleInputFocus = event => {
+      setIsFocused(true)
+      safeInvoke(onFocus, event)
+    }
+
+    const handleKeyDown = event => {
+      const { selectionEnd, value } = event.target
+      const key = GET_KEY_FOR_TAG_DELIMITER[tagSubmitKey]
+
+      if (event.key === key) {
+        event.preventDefault()
+        addTags(value)
+      } else if (event.key === 'Backspace' && selectionEnd === 0) {
+        handleBackspaceToRemove(event)
+      }
+    }
+
+    const handleRemoveTag = event => {
+      // Using data attribute to simplify callback logic -- one handler for all children
+      const index = Number(
+        event.currentTarget.parentElement.getAttribute('data-tag-index')
+      )
+      removeTagAtIndex(index)
+    }
+
+    const maybeRenderTag = (tag, index) => {
+      if (!tag) {
+        return null
+      }
+
+      const propsForElement = safeInvoke(tagProps, tag, index) || tagProps
+
+      return (
+        <Tag
+          key={`${tag}:${index}`}
+          data-tag-index={index}
+          marginRight={majorScale(1)}
+          marginY="6px"
+          onRemove={disabled ? null : handleRemoveTag}
+          isRemovable={!disabled}
+          {...propsForElement}
+        >
+          {tag}
+        </Tag>
+      )
+    }
+
+    const themedContainerClassName = theme.getTagInputClassName('default')
+    const themedInputClassName = theme.getTextInputClassName('none')
+    const textSize = theme.getTextSizeForControlHeight(height)
+    const borderRadius = theme.getBorderRadiusForControlHeight(height)
 
     return (
-      <Tag
-        key={`${tag}:${index}`}
-        data-tag-index={index}
-        marginRight={majorScale(1)}
-        marginY="6px"
-        onRemove={disabled ? null : handleRemoveTag}
-        isRemovable={!disabled}
-        {...propsForElement}
+      <Box
+        aria-disabled={disabled || undefined}
+        aria-activedescendant={isFocused ? id : undefined}
+        borderRadius={borderRadius}
+        className={cx(themedContainerClassName, className)}
+        paddingLeft={Math.round(height / 3.2)}
+        paddingRight={Math.round(height / 3.2)}
+        paddingY="2px"
+        ref={ref}
+        {...rest}
+        onBlur={handleBlur}
       >
-        {tag}
-      </Tag>
+        {values.map(maybeRenderTag)}
+        <Text
+          is="input"
+          id={id}
+          color={disabled ? 'muted' : undefined}
+          disabled={disabled}
+          flexGrow="1"
+          height={height - 4}
+          size={textSize}
+          type="text"
+          value={inputValue}
+          {...inputProps}
+          className={themedInputClassName}
+          ref={inputRef}
+          onChange={handleInputChange}
+          onFocus={handleInputFocus}
+          onKeyDown={handleKeyDown}
+        />
+      </Box>
     )
-  }
-
-  const themedContainerClassName = theme.getTagInputClassName('default')
-  const themedInputClassName = theme.getTextInputClassName('none')
-  const textSize = theme.getTextSizeForControlHeight(height)
-  const borderRadius = theme.getBorderRadiusForControlHeight(height)
-
-  return (
-    <Box
-      aria-disabled={disabled || undefined}
-      aria-activedescendant={isFocused ? id : undefined}
-      borderRadius={borderRadius}
-      className={cx(themedContainerClassName, className)}
-      paddingLeft={Math.round(height / 3.2)}
-      paddingRight={Math.round(height / 3.2)}
-      paddingY="2px"
-      {...rest}
-      onBlur={handleBlur}
-    >
-      {values.map(maybeRenderTag)}
-      <Text
-        is="input"
-        id={id}
-        color={disabled ? 'muted' : undefined}
-        disabled={disabled}
-        flexGrow="1"
-        height={height - 4}
-        size={textSize}
-        type="text"
-        value={inputValue}
-        {...inputProps}
-        className={themedInputClassName}
-        ref={inputRef}
-        onChange={handleInputChange}
-        onFocus={handleInputFocus}
-        onKeyDown={handleKeyDown}
-      />
-    </Box>
-  )
-})
+  })
+)
 
 TagInput.propTypes = {
   /** Whether or not the inputValue should be added to the tags when the input blurs. */
