c191f78179a72adfce9b1eb294b46c67b3e1dc82
Vadim Demedes <vdemedes@gmail.com>
2020-08-10 21:42:37 +0300

    Call `onStateChange` if passed to `<Combobox>` (#926)

--- src/combobox/stories/index.stories.js
+++ src/combobox/stories/index.stories.js
@@ -1,5 +1,5 @@
 import { storiesOf } from '@storybook/react'
-import React from 'react'
+import React, { useState } from 'react'
 import Box from 'ui-box'
 import starWarsNames from 'starwars-names'
 import { Heading } from '../../typography'
@@ -27,95 +27,115 @@ const handleChange = selectedItem => {
   console.log(selectedItem)
 }
 
-storiesOf('combobox', module).add('Combobox', () => (
-  <Box padding={40}>
-    {(() => {
-      document.body.style.margin = '0'
-      document.body.style.height = '100vh'
-    })()}
-    <Box marginBottom={16}>
-      <Heading>Default usage</Heading>
-      <Combobox items={items} onChange={handleChange} />
-    </Box>
-    <Box marginBottom={16}>
-      <Heading>Custom width</Heading>
-      <Combobox width={120} items={items} onChange={handleChange} />
-    </Box>
-    <Box marginBottom={16} marginLeft={400}>
-      <Heading>Custom width + offset</Heading>
-      <Combobox width={120} items={items} onChange={handleChange} />
-    </Box>
-    <Box marginBottom={16}>
-      <Heading>Open on focus</Heading>
-      <Combobox openOnFocus items={items} onChange={handleChange} />
-    </Box>
-    <Box marginBottom={16}>
-      <Heading>Default value</Heading>
-      <Combobox
-        initialSelectedItem="Yoda"
-        items={items}
-        onChange={handleChange}
-      />
-    </Box>
-    <Box marginBottom={16}>
-      <Heading>Custom item objects</Heading>
-      <Combobox
-        initialSelectedItem={customItems[0]}
-        items={customItems}
-        itemToString={i => (i ? i.label : '')}
-        onChange={handleChange}
-      />
-    </Box>
-    <Box marginBottom={16}>
-      <Heading>Disabled usage</Heading>
-      <Combobox items={items} onChange={handleChange} disabled />
-    </Box>
-    <Box marginBottom={16}>
-      <Heading>Loading usage</Heading>
-      <Combobox items={items} onChange={handleChange} isLoading />
-    </Box>
+storiesOf('combobox', module).add('Combobox', () => {
+  const [value, setValue] = useState('')
 
-    <Box marginBottom={16}>
-      <Heading>Full width combobox</Heading>
-
-      <Pane display="flex" background="tint1" padding={16}>
+  return (
+    <Box padding={40}>
+      {(() => {
+        document.body.style.margin = '0'
+        document.body.style.height = '100vh'
+      })()}
+      <Box marginBottom={16}>
+        <Heading>Default usage</Heading>
+        <Combobox items={items} onChange={handleChange} />
+      </Box>
+      <Box marginBottom={16}>
+        <Heading>Custom width</Heading>
+        <Combobox width={120} items={items} onChange={handleChange} />
+      </Box>
+      <Box marginBottom={16} marginLeft={400}>
+        <Heading>Custom width + offset</Heading>
+        <Combobox width={120} items={items} onChange={handleChange} />
+      </Box>
+      <Box marginBottom={16}>
+        <Heading>Open on focus</Heading>
+        <Combobox openOnFocus items={items} onChange={handleChange} />
+      </Box>
+      <Box marginBottom={16}>
+        <Heading>Default value</Heading>
         <Combobox
-          width="100%"
+          initialSelectedItem="Yoda"
+          items={items}
+          onChange={handleChange}
+        />
+      </Box>
+      <Box marginBottom={16}>
+        <Heading>Any value</Heading>
+        <Combobox
+          items={items}
+          selectedItem={value}
+          autocompleteProps={{
+            onStateChange: changes => {
+              if (changes.selectedItem) {
+                setValue(changes.selectedItem)
+              } else if (changes.inputValue) {
+                setValue(changes.inputValue)
+              }
+            }
+          }}
+        />
+      </Box>
+      <Box marginBottom={16}>
+        <Heading>Custom item objects</Heading>
+        <Combobox
           initialSelectedItem={customItems[0]}
           items={customItems}
           itemToString={i => (i ? i.label : '')}
           onChange={handleChange}
         />
-      </Pane>
+      </Box>
+      <Box marginBottom={16}>
+        <Heading>Disabled usage</Heading>
+        <Combobox items={items} onChange={handleChange} disabled />
+      </Box>
+      <Box marginBottom={16}>
+        <Heading>Loading usage</Heading>
+        <Combobox items={items} onChange={handleChange} isLoading />
+      </Box>
 
-      <Heading>Pane has 75% width</Heading>
-      <Pane display="flex" background="tint2" width="75%" padding={16}>
-        <Combobox
-          width="100%"
-          initialSelectedItem={customItems[0]}
-          items={customItems}
-          itemToString={i => (i ? i.label : '')}
-          onChange={handleChange}
-        />
-      </Pane>
+      <Box marginBottom={16}>
+        <Heading>Full width combobox</Heading>
 
-      <Heading>
-        Pane is a column flexbox and Combobox is set to 100% width
-      </Heading>
-      <Pane
-        flexDirection="column"
-        display="flex"
-        background="greenTint"
-        padding={16}
-      >
-        <Combobox
-          width="100%"
-          initialSelectedItem={customItems[0]}
-          items={customItems}
-          itemToString={i => (i ? i.label : '')}
-          onChange={handleChange}
-        />
-      </Pane>
+        <Pane display="flex" background="tint1" padding={16}>
+          <Combobox
+            width="100%"
+            initialSelectedItem={customItems[0]}
+            items={customItems}
+            itemToString={i => (i ? i.label : '')}
+            onChange={handleChange}
+          />
+        </Pane>
+
+        <Heading>Pane has 75% width</Heading>
+        <Pane display="flex" background="tint2" width="75%" padding={16}>
+          <Combobox
+            width="100%"
+            initialSelectedItem={customItems[0]}
+            items={customItems}
+            itemToString={i => (i ? i.label : '')}
+            onChange={handleChange}
+          />
+        </Pane>
+
+        <Heading>
+          Pane is a column flexbox and Combobox is set to 100% width
+        </Heading>
+        <Pane
+          flexDirection="column"
+          display="flex"
+          background="greenTint"
+          padding={16}
+        >
+          <Combobox
+            width="100%"
+            initialSelectedItem={customItems[0]}
+            items={customItems}
+            itemToString={i => (i ? i.label : '')}
+            onChange={handleChange}
+          />
+        </Pane>
+      </Box>
     </Box>
-  </Box>
-))
+  )
+})
