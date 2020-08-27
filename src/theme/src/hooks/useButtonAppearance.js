91231eafb2fd66f2cf5256374596ebb129c428a5
colinlohner <colin@colinlohner.com>
2020-08-27 13:16:42 -0700

    Start theme reorg
    - Renamed themes (v5 -> Legacy, v6 -> Default)
    - Updated button to be token aware

--- src/theme/src/hooks/useButtonAppearance.js
+++ src/theme/src/hooks/useButtonAppearance.js
@@ -25,7 +25,90 @@ const activeState =
   '&:not([disabled]):not([data-disabled]):active, &:not([disabled]):not([data-disabled])[aria-expanded="true"], &:not([disabled]):not([data-disabled])[data-active]'
 
 function getButtonStyles(theme, appearance) {
-  const { buttons } = theme
+  const { buttons: themeStyles, tokens } = theme
+
+  const defaultStyles = {
+    primary: {
+      base: {
+        backgroundColor: tokens.primary.base,
+        borderColor: tokens.primary.base,
+        color: 'white'
+      },
+      disabled: { ...disabled },
+      hover: {
+        backgroundColor: tokens.primary.hover
+      },
+      focus: {
+        backgroundColor: tokens.primary.hover,
+        boxShadow: `0 0 0 2px ${tokens.colors.blue100}`
+      },
+      active: {
+        backgroundColor: tokens.primary.active
+      }
+    },
+    default: {
+      base: {
+        backgroundColor: 'white',
+        border: `1px solid ${tokens.colors.gray500}`,
+        color: tokens.colors.gray800
+      },
+      disabled: {
+        ...disabled,
+        color: tokens.colors.gray500,
+        borderColor: tokens.colors.gray300
+      },
+      hover: {
+        borderColor: tokens.colors.gray600,
+        backgroundColor: tokens.colors.gray50
+      },
+      focus: {
+        boxShadow: `0 0 0 2px ${tokens.colors.blue100}`
+      },
+      active: {
+        backgroundColor: tokens.colors.gray100
+      },
+      focusAndActive: {}
+    },
+    destructive: {
+      base: {
+        backgroundColor: tokens.colors.red500,
+        color: 'white'
+      },
+      disabled: {
+        ...disabled
+      },
+      hover: {
+        backgroundColor: tokens.colors.red600,
+        boxShadow: `0 0 0 2px ${tokens.colors.red100}`
+      },
+      active: {
+        backgroundColor: tokens.colors.red700
+      }
+    },
+    minimal: {
+      base: {
+        backgroundColor: 'transparent',
+        color: tokens.colors.gray800
+      },
+      focus: {
+        boxShadow: `0 0 0 2px ${tokens.colors.blue100}`
+      },
+      disabled: {
+        ...disabled,
+        color: tokens.colors.gray500,
+        borderColor: tokens.colors.gray300
+      },
+      hover: {
+        backgroundColor: tokens.colors.gray100
+      },
+      active: {
+        backgroundColor: tokens.colors.gray200
+      },
+      focusAndActive: {}
+    }
+  }
+
+  const buttonStyles = {...defaultStyles, ...themeStyles}
 
   const {
     base: baseStyles = {},
@@ -34,7 +117,7 @@ function getButtonStyles(theme, appearance) {
     focus = {},
     active = {},
     focusAndActive = {}
-  } = (buttons || {})[appearance] || {}
+  } = (buttonStyles || {})[appearance] || {}
 
   return {
     ...base,
