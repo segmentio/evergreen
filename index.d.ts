47717d291089adc7fcf8a88057d1b270bab45891
Matt Shwery <mshwery@gmail.com>
2020-08-31 14:50:37 -0500

    add StackingOrder and Code appearance typedefs

--- index.d.ts
+++ index.d.ts
@@ -618,7 +618,7 @@ export interface CheckboxOwnProps {
 export type CheckboxProps = PolymorphicBoxProps<'input', CheckboxOwnProps>
 export declare const Checkbox: BoxComponent<CheckboxOwnProps, 'input'>
 
-export type CodeOwnProps = TextOwnProps
+export type CodeOwnProps = TextOwnProps & { appearance?: 'default' | 'minimal' }
 export type CodeProps = PolymorphicBoxProps<'code', CodeOwnProps>
 export declare const Code: BoxComponent<CodeOwnProps, 'code'>
 
@@ -1629,6 +1629,14 @@ export interface StackProps {
 export declare const Stack: React.FC<StackProps>
 
 export declare const StackingContext: React.Context<number>
+
+export const StackingOrder: {
+  FOCUSED: number
+  STACKING_CONTEXT: number
+  POSITIONER: number
+  OVERLAY: number
+  TOASTER: number
+}
 
 export type StrongOwnProps = TextOwnProps
 export type StrongProps = PolymorphicBoxProps<'strong', StrongOwnProps>
