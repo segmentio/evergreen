76516b95c24017ae7e673004dc90b1a165c381b5
Matt Shwery <matthew.shwery@segment.com>
2020-09-09 12:47:24 -0500

    upgrade to storybook v6 and provide theme toolbar for all stories (#983)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,7 @@
 module.exports = {
   stories: ['../**/*.stories.js'],
   addons: [
+    '@storybook/addon-essentials',
     '@storybook/addon-actions',
     '@storybook/addon-links',
     '@storybook/addon-storysource'
