565554671423903a24e7f9c045ed36bd7e1b9bc1
Allen Kleiner <akleiner24@gmail.com>
2020-09-10 18:01:12 -0700

    update

--- src/themes/classic/components/button.js
+++ src/themes/classic/components/button.js
@@ -1,6 +1,80 @@
-const baseStyle = {}
-const appearances = {}
-const sizes = {}
+import { getPrimaryButtonStylesForIntent } from '../deprecated/helpers'
+import { defaultControlStyles } from '../deprecated/shared'
+
+const baseStyle = {
+  fontFamily: 'fontFamilies.ui',
+  borderRadius: 'radii.1',
+  border: 0
+}
+
+const appearances = {
+  primary: {
+    backgroundColor: 'white',
+    backgroundImage: (_, props) =>
+      getPrimaryButtonStylesForIntent(props.intent).linearGradient.base,
+    boxShadow: theme =>
+      `inset 0 0 0 1px ${theme.scales.neutral.N5A}, inset 0 -1px 1px 0 ${theme.scales.neutral.N2A}`,
+    color: 'white',
+    _focus: {
+      backgroundImage: (_, props) =>
+        getPrimaryButtonStylesForIntent(props.intent).linearGradient.focus,
+      boxShadow: (theme, props) =>
+        `0 0 0 3px ${
+          getPrimaryButtonStylesForIntent(props.intent).linearGradient
+            .focusColor
+        }, inset 0 0 0 1px ${theme.scales.neutral.N4A}, inset 0 -1px 1px 0 ${
+          theme.scales.neutral.N5A
+        }`
+    },
+    _hover: {
+      backgroundImage: (_, props) =>
+        getPrimaryButtonStylesForIntent(props.intent).linearGradient.hover
+    },
+    _active: {
+      backgroundImage: (_, props) =>
+        getPrimaryButtonStylesForIntent(props.intent).linearGradient.active
+    },
+    _focusAndActive: {}
+  },
+
+  default: {
+    ...defaultControlStyles.base,
+    _hover: {
+      ...defaultControlStyles.hover
+    },
+    _active: {
+      ...defaultControlStyles.active
+    },
+    _disabled: {
+      ...defaultControlStyles.disabled
+    }
+  },
+  minimal: {}
+}
+
+const sizes = {
+  small: {
+    height: 24,
+    minWidth: 24,
+    lineHeight: '24px',
+    paddingLeft: 12,
+    paddingRight: 12
+  },
+  medium: {
+    height: 32,
+    minWidth: 32,
+    lineHeight: '32px',
+    paddingLeft: 16,
+    paddingRight: 16
+  },
+  large: {
+    height: 40,
+    minWidth: 40,
+    lineHeight: '40px',
+    paddingLeft: 20,
+    paddingRight: 20
+  }
+}
 
 export default {
   baseStyle,
--- src/themes/default/components/button.js
+++ src/themes/default/components/button.js
@@ -1,6 +1,113 @@
-const baseStyle = {}
-const appearances = {}
-const sizes = {}
+const baseStyle = {
+  fontFamily: 'fontFamilies.ui',
+  border: '1px solid transparent',
+  borderRadius: 'radii.1'
+}
+
+const colorKeyForAppearance = appearance =>
+  appearance === 'destructive' ? 'red' : 'blue'
+
+const getPrimaryButtonAppearance = appearance => {
+  const color = colorKeyForAppearance(appearance)
+  return {
+    backgroundColor: `colors.${color}500`,
+    borderColor: `colors.${color}500`,
+    color: 'white',
+    _hover: {
+      backgroundColor: `colors.${color}600`,
+      borderColor: `colors.${color}600`
+    },
+    _disabled: {
+      backgroundColor: `colors.${color}100`,
+      cursor: 'not-allowed',
+      borderColor: `colors.${color}100`
+    },
+    _focus: {
+      backgroundColor: `colors.${color}500`,
+      boxShadow: theme => `0 0 0 2px ${theme.colors[`${color}100`]}`,
+      borderColor: `colors.${color}500`
+    },
+    _active: {
+      backgroundColor: `colors.${color}700`,
+      borderColor: `colors.${color}700`
+    }
+  }
+}
+
+const appearances = {
+  primary: getPrimaryButtonAppearance(),
+  default: {
+    backgroundColor: 'white',
+    border: theme => `1px solid ${theme.colors.gray500}`,
+    color: 'colors.gray800',
+
+    _disabled: {
+      cursor: 'not-allowed',
+      color: 'colors.gray500',
+      borderColor: 'colors.gray300'
+    },
+
+    _hover: {
+      borderColor: 'colors.gray600',
+      backgroundColor: 'colors.gray50'
+    },
+
+    _focus: {
+      boxShadow: theme => `0 0 0 2px ${theme.colors.blue100}`
+    },
+
+    _active: {
+      backgroundColor: 'colors.gray100'
+    }
+  },
+  minimal: {
+    backgroundColor: 'transparent',
+    color: 'colors.gray800',
+
+    _focus: {
+      boxShadow: theme => `0 0 0 2px ${theme.colors.blue100}`
+    },
+
+    _disabled: {
+      cursor: 'not-allowed',
+      color: 'colors.gray500',
+      borderColor: 'colors.gray300'
+    },
+
+    _hover: {
+      backgroundColor: 'colors.gray100'
+    },
+
+    _active: {
+      backgroundColor: 'colors.gray200'
+    }
+  },
+  destructive: getPrimaryButtonAppearance('destructive')
+}
+
+const sizes = {
+  small: {
+    height: 24,
+    minWidth: 24,
+    lineHeight: '24px',
+    paddingLeft: 12,
+    paddingRight: 12
+  },
+  medium: {
+    height: 32,
+    minWidth: 32,
+    lineHeight: '32px',
+    paddingLeft: 16,
+    paddingRight: 16
+  },
+  large: {
+    height: 40,
+    minWidth: 40,
+    lineHeight: '40px',
+    paddingLeft: 20,
+    paddingRight: 20
+  }
+}
 
 export default {
   baseStyle,
