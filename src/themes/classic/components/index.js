22505f1df0d629da45a5ffde93aa7a1b4c20d426
Allen Kleiner <akleiner24@gmail.com>
2020-09-10 20:55:46 -0700

    v6 buttons in new style paradigm (#995)
    * update
    
    * button updates
    
    * goodbye usebuttonappearnce
    
    * get select done too
    
    * update snaps
    
    * temporarily bump size-limit :(

--- src/themes/classic/components/index.js
+++ src/themes/classic/components/index.js
@@ -1,9 +1,11 @@
 import Alert from './alert'
 import Button from './button'
 import Code from './code'
+import Select from './select'
 
 export default {
   Alert,
   Button,
-  Code
+  Code,
+  Select
 }
