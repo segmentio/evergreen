d4f2562483e870b96c284ace47ac2abf1a3b7ee7
Matt Shwery <mshwery@gmail.com>
2020-09-14 14:10:09 -0500

    tweak input styles

--- src/themes/classic/components/input.js
+++ src/themes/classic/components/input.js
@@ -1,7 +1,17 @@
 const baseStyle = {
   borderRadius: 'radii.1',
   fontFamily: 'fontFamilies.ui',
-  lineHeight: '12px'
+  lineHeight: '12px',
+  color: 'colors.default',
+
+  _placeholder: {
+    color: 'colors.neutralAlpha.N6A'
+  },
+
+  _disabled: {
+    cursor: 'not-allowed',
+    backgroundColor: 'colors.neutralAlpha.N2'
+  }
 }
 
 const appearances = {
@@ -14,18 +24,12 @@ const appearances = {
       boxShadow: theme =>
         `inset 0 0 0 1px ${theme.palette.red.base}, inset 0 1px 2px ${theme.scales.neutral.N4A}`
     },
-    _placeholder: {
-      color: 'scales.neutral.N6A'
-    },
     _focus: {
-      outline: 'none',
       boxShadow: theme =>
         `inset 0 0 2px ${theme.scales.neutral.N4A}, inset 0 0 0 1px ${theme.scales.blue.B7}, ${theme.shadows.focusRing}`
     },
     _disabled: {
-      cursor: 'not-allowed',
-      boxShadow: theme => `inset 0 0 0 1px ${theme.scales.neutral.N4A}`,
-      backgroundColor: 'scales.neutral.N2'
+      boxShadow: theme => `inset 0 0 0 1px ${theme.scales.neutral.N4A}`
     }
   },
   neutral: {
@@ -33,33 +37,16 @@ const appearances = {
     _invalid: {
       boxShadow: theme => `inset 0 0 0 1px ${theme.palette.red.base}`
     },
-    _placeholder: {
-      color: 'scales.neutral.N6A'
-    },
     _focus: {
-      outline: 'none',
       backgroundColor: 'white',
       boxShadow: theme => `0 0 0 2px ${theme.scales.blue.B6A}`
     },
     _disabled: {
-      cursor: 'not-allowed',
-      boxShadow: theme => `inset 0 0 0 1px ${theme.scales.neutral.N4A}`,
-      backgroundColor: 'scales.neutral.N2'
+      boxShadow: theme => `inset 0 0 0 1px ${theme.scales.neutral.N4A}`
     }
   },
   none: {
-    backgroundColor: 'white',
-    _invalid: {},
-    _placeholder: {
-      color: 'scales.neutral.N6A'
-    },
-    _focus: {
-      outline: 'none'
-    },
-    _disabled: {
-      cursor: 'not-allowed',
-      backgroundColor: 'scales.neutral.N2'
-    }
+    backgroundColor: 'white'
   }
 }
 
