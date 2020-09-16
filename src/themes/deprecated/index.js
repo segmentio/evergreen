fcc97740f5de89d43f228f990e079cea9985eb4b
Allen Kleiner <akleiner24@gmail.com>
2020-09-16 14:17:51 -0700

    Add a `defaultDeprecatedTheme` export that just composes fills, colors, palette, etc

---
+++ src/themes/deprecated/index.js
@@ -0,0 +1,11 @@
+import colors from './foundational-styles/colors'
+import fills from './foundational-styles/fills'
+import palette from './foundational-styles/palette'
+import scales from './foundational-styles/scales'
+
+export default {
+  scales,
+  fills,
+  colors,
+  palette
+}
