293d8f553db1d2facbf63165d90d4ef6210c54c4
Allen Kleiner <akleiner24@gmail.com>
2020-09-24 05:25:30 -0700

    Fix numeric border box prop in the button classic theme

--- src/themes/classic/components/button.js
+++ src/themes/classic/components/button.js
@@ -7,7 +7,7 @@ import { defaultControlStyles } from '../deprecated/shared'
 const baseStyle = {
   fontFamily: 'fontFamilies.ui',
   borderRadius: 'radii.1',
-  border: 0,
+  border: '0',
   color: (theme, { color }) => theme.colors[color] || color || 'colors.default',
   _disabled: {
     ...defaultControlStyles.disabled
