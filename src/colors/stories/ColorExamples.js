d7db9eefd0a6b83f160e0732cb3106dd6b2fdc18
wangcch <wangcch.cc@gmail.com>
2020-09-24 14:07:07 +0800

    docs(colors): support v6 (obj / string)

--- src/colors/stories/ColorExamples.js
+++ src/colors/stories/ColorExamples.js
@@ -3,59 +3,74 @@ import { Pane } from '../../layers'
 import { ThemeConsumer } from '../../theme'
 import { Heading } from '../../typography'
 import ColorGroup from './ColorGroup'
+import Swatch from './Swatch'
 
 function capitalize(string) {
   return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase()
 }
+
+const ColorViewer = props => {
+  const { colorValue, name } = props
+
+  if (typeof colorValue === 'string') {
+    return (
+      <Swatch
+        color={colorValue}
+        name={name}
+        property={`theme.colors.${name}`}
+      />
+    )
+  } else {
+    return (
+      <ColorGroup
+        title={capitalize(name)}
+        colorGroup={colorValue}
+        name={childKey => `theme.colors.${name}.${childKey}`}
+      />
+    )
+  }
+}
+
+const colorKeys = [
+  {
+    key: 'palette',
+    name: 'Palette'
+  },
+  {
+    key: 'colors',
+    name: 'Function colors'
+  },
+  {
+    key: 'scales',
+    name: 'Scales'
+  }
+]
 
 const ColorExamples = props => {
   return (
     <ThemeConsumer>
       {theme => (
         <Pane {...props}>
-          <Pane clearfix>
-            <Heading size={800}>Palette</Heading>
-            {Object.keys(theme.palette).map(key => {
+          {colorKeys.map(item => {
+            if (!theme[item.key]) {
+              return null
+            } else {
               return (
-                <ColorGroup
-                  key={key}
-                  title={capitalize(key)}
-                  colorGroup={theme.palette[key]}
-                  name={childKey => `theme.palette.${key}.${childKey}`}
-                />
+                <Pane clearfix key={item.key} marginBottom={24}>
+                  <Heading size={800}>{capitalize(item.name)}</Heading>
+                  {Object.keys(theme[item.key]).map(key => {
+                    return (
+                      <ColorViewer
+                        key={key}
+                        name={key}
+                        colorValue={theme[item.key][key]}
+                      />
+                    )
+                  })}
+                </Pane>
               )
-            })}
-          </Pane>
-          <Pane clearfix>
-            <Heading size={800} marginTop={24}>
-              Functional Colors
-            </Heading>
-            {Object.keys(theme.colors).map(key => {
-              return (
-                <ColorGroup
-                  key={key}
-                  title={capitalize(key)}
-                  colorGroup={theme.colors[key]}
-                  name={childKey => `theme.colors.${key}.${childKey}`}
-                />
-              )
-            })}
-          </Pane>
-          <Pane clearfix>
-            <Heading size={800} marginTop={24}>
-              Scales
-            </Heading>
-            {Object.keys(theme.scales).map(key => {
-              return (
-                <ColorGroup
-                  key={key}
-                  title={capitalize(key)}
-                  colorGroup={theme.scales[key]}
-                  name={childKey => `theme.scales.${key}.${childKey}`}
-                />
-              )
-            })}
-          </Pane>
+            }
+          })}
         </Pane>
       )}
     </ThemeConsumer>
