66a5680bee87f0bedcd483e3d8a73b6565b91de9
Yue Yang <g1enyy0ung@gmail.com>
2020-09-27 14:02:01 +0800

    fix: ci
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,9 @@
 import babel from '@rollup/plugin-babel'
 import commonjs from '@rollup/plugin-commonjs'
-import pkg from './package.json'
 import replace from '@rollup/plugin-replace'
 import resolve from '@rollup/plugin-node-resolve'
 import { terser } from 'rollup-plugin-terser'
+import pkg from './package.json'
 
 // Use peerDependencies as rollup external
 // https://rollupjs.org/guide/en/#peer-dependencies
