5c3dc77ac5016224fcce3dc3067299fa56cc6d2a
Yue Yang <g1enyy0ung@gmail.com>
2020-09-27 13:49:50 +0800

    chore: upgrade rollup
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,14 @@
-import babel from 'rollup-plugin-babel'
+import babel from '@rollup/plugin-babel'
 import commonjs from '@rollup/plugin-commonjs'
+import pkg from './package.json'
 import replace from '@rollup/plugin-replace'
 import resolve from '@rollup/plugin-node-resolve'
 import { terser } from 'rollup-plugin-terser'
-import pkg from './package.json'
 
 // Use peerDependencies as rollup external
 // https://rollupjs.org/guide/en/#peer-dependencies
 const { peerDependencies } = pkg
 const external = Object.keys(peerDependencies)
-
-// Explicitly specify unresolvable named exports.
-// https://github.com/rollup/plugins/tree/master/packages/commonjs#namedexports
-const namedExports = {
-  'node_modules/react-is/index.js': ['isForwardRef'],
-  'node_modules/prop-types/index.js': [
-    'array',
-    'arrayOf',
-    'func',
-    'number',
-    'object',
-    'oneOf',
-    'oneOfType',
-    'string'
-  ]
-}
 
 // Ignore SSR imports in UMD, replace with empty functions
 const ignoreSSR = {
@@ -53,12 +37,11 @@ export default [
       }),
       resolve(),
       commonjs({
-        include: 'node_modules/**',
-        namedExports
+        include: 'node_modules/**'
       }),
       babel({
         exclude: 'node_modules/**',
-        runtimeHelpers: true
+        babelHelpers: 'runtime'
       })
     ]
   },
@@ -83,12 +66,11 @@ export default [
       }),
       resolve(),
       commonjs({
-        include: 'node_modules/**',
-        namedExports
+        include: 'node_modules/**'
       }),
       babel({
         exclude: 'node_modules/**',
-        runtimeHelpers: true
+        babelHelpers: 'runtime'
       }),
       terser()
     ]
