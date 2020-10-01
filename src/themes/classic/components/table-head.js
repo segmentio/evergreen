e9873f50432231eecbad54d370899aecc7208d14
Colin Lohner <colin@colinlohner.com>
2020-10-01 13:30:29 -0700

    Update Table Head to appear as expected in themes (#1077)

--- src/themes/classic/components/table-head.js
+++ src/themes/classic/components/table-head.js
@@ -1,6 +1,13 @@
 const baseStyle = {
   borderBottom: 'default',
-  background: 'colors.tint2'
+  background: 'colors.tint2',
+  height: '32px',
+  fontSize: 'fontSizes.1',
+  fontWeight: 'fontWeights.semibold',
+  lineHeight: 'lineHeights.0',
+  letterSpacing: 'letterSpacings.normal',
+  fontFamily: 'fontFamilies.ui',
+  color: 'colors.default'
 }
 
 const appearances = {}
--- src/themes/default/components/table-head.js
+++ src/themes/default/components/table-head.js
@@ -1,6 +1,14 @@
 const baseStyle = {
   borderBottom: 'default',
-  background: 'colors.tint2'
+  background: 'colors.tint2',
+  height: '56px',
+  fontSize: 'fontSizes.1',
+  fontWeight: 'fontWeights.bold',
+  lineHeight: 'lineHeights.0',
+  letterSpacing: 'letterSpacings.normal',
+  fontFamily: 'fontFamilies.ui',
+  color: 'colors.muted',
+  textTransform: 'uppercase'
 }
 
 const appearances = {}
