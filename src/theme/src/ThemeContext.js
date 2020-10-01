8eaa8966900ece26ec283c3824802952328a3e85
Allen Kleiner <akleiner24@gmail.com>
2020-10-01 11:23:10 -0700

    Make classic theme the fallback theme coming from the theme context (#1074)
    * Make classic theme the fallback theme coming from the theme context
    
    * update snaps
    
    * bump size limit for now

--- src/theme/src/ThemeContext.js
+++ src/theme/src/ThemeContext.js
@@ -1,10 +1,14 @@
 import React from 'react'
-import defaultTheme from '../../themes/default'
+import classicTheme from '../../themes/classic'
 
 /**
  * Use React 16.3+ createContext API.
  */
-const ThemeContext = React.createContext(defaultTheme)
+
+// NOTE(allen) - switch this back once we properly refactor Toasts to render
+// them in the existing DOM tree flow, instead of mounting a new root
+// outside of whatever app root an EG consumer is using.
+const ThemeContext = React.createContext(classicTheme)
 const { Consumer: ThemeConsumer, Provider: ThemeProvider } = ThemeContext
 
 export default ThemeContext
