0d76225e2bdfff7e12106c10c14ed9b5b02c7a2f
Allen Kleiner <akleiner24@gmail.com>
2020-10-06 08:41:35 -0700

    Bring default theme headings + font weights closer to spec  (#1083)
    * Make all headings 600 font-weight in the default theme
    
    * updates

--- src/themes/default/components/heading.js
+++ src/themes/default/components/heading.js
@@ -10,46 +10,48 @@ const sizes = {
   900: {
     fontSize: 'fontSizes.7',
     lineHeight: 'lineHeights.6',
+    fontWeight: 'fontWeights.bold',
     letterSpacing: 'letterSpacings.tightest'
   },
   800: {
     fontSize: 'fontSizes.6',
     lineHeight: 'lineHeights.5',
+    fontWeight: 'fontWeights.bold',
     letterSpacing: 'letterSpacings.tightest'
   },
   700: {
     fontSize: 'fontSizes.5',
     lineHeight: 'lineHeights.3',
+    fontWeight: 'fontWeights.bold',
     letterSpacing: 'letterSpacings.tighter'
   },
   600: {
     fontSize: 'fontSizes.4',
     lineHeight: 'lineHeights.3',
+    fontWeight: 'fontWeights.bold',
     letterSpacing: 'letterSpacings.tighter'
   },
   500: {
     fontFamily: 'fontFamilies.ui',
     fontSize: 'fontSizes.3',
+    fontWeight: 'fontWeights.bold',
     letterSpacing: 'letterSpacings.tight',
     lineHeight: 'lineHeights.2'
   },
   400: {
     fontSize: 'fontSizes.2',
-    fontWeight: 'fontWeights.bold',
     lineHeight: 'lineHeights.1',
     letterSpacing: 'letterSpacings.tight',
     fontFamily: 'fontFamilies.ui'
   },
   300: {
     fontSize: 'fontSizes.1',
-    fontWeight: 'fontWeights.bold',
     lineHeight: 'lineHeights.0',
     letterSpacing: 'letterSpacings.normal',
     fontFamily: 'fontFamilies.ui'
   },
   200: {
     fontSize: 'fontSizes.1',
-    fontWeight: 'fontWeights.bold',
     lineHeight: 'lineHeights.0',
     letterSpacing: 'letterSpacings.normal',
     fontFamily: 'fontFamilies.ui',
@@ -57,7 +59,6 @@ const sizes = {
   },
   100: {
     fontSize: 'fontSizes.0',
-    fontWeight: 'fontWeights.normal',
     textTransform: 'uppercase',
     lineHeight: 'lineHeights.0',
     letterSpacing: 'letterSpacings.wide',
