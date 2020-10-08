e9f7cf4f399a63e6777de26534eabca099c60d81
Colin Lohner <colin@colinlohner.com>
2020-10-08 11:45:32 -0700

    Migrate `<Label />` away from using `<Text />` (#1091)
    * Migrate `<Label />` away from using `<Text />`
    
    * Decouple styling of Label from Heading

--- src/typography/src/Label.js
+++ src/typography/src/Label.js
@@ -1,14 +1,49 @@
 import React, { memo, forwardRef } from 'react'
-import Text from './Text'
+import cx from 'classnames'
+import PropTypes from 'prop-types'
+import Box from 'ui-box'
+import { useStyleConfig } from '../../hooks'
+
+const pseudoSelectors = {}
+const internalStyles = {}
 
 const Label = memo(
   forwardRef(function Label(props, ref) {
-    return <Text is="label" fontWeight={500} {...props} ref={ref} />
+    const {
+      className,
+      size = 400,
+      ...restProps
+    } = props
+
+    const { className: themedClassName, ...boxProps } = useStyleConfig(
+      'Label',
+      { size },
+      pseudoSelectors,
+      internalStyles
+    )
+
+    return  (
+      <Box
+        is="label"
+        ref={ref}
+        className={cx(themedClassName, className)}
+        {...boxProps}
+        {...restProps}
+      />
+    )
   })
 )
 
 Label.propTypes = {
-  ...Text.propTypes
+  /**
+   * Label composes Box as the base.
+   */
+  ...Box.propTypes,
+
+  /**
+   * The size of the label.
+   */
+  size: PropTypes.oneOf([100, 200, 300, 400, 500, 600, 700, 800, 900])
 }
 
 export default Label
