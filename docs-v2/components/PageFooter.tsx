fa36412bbaee25b8ea861c46e2e3348163e29a74
Kate Hsiao <kamebkj@gmail.com>
2021-04-22 14:26:41 -0700

    Add back consent manager

--- docs-v2/components/PageFooter.tsx
+++ docs-v2/components/PageFooter.tsx
@@ -1,11 +1,7 @@
 import React from 'react'
 import { openConsentManager } from '@segment/consent-manager'
-// import SegmentLogoWordmark from './SegmentLogoWordmark'
-import { majorScale, Pane, Text, Link, Heading} from 'evergreen-ui'
+import { majorScale, Pane, Text, Link} from 'evergreen-ui'
 
-// const NativeLink = (props: any) => (
-//   <a target="_blank" rel="noopener noreferrer" {...props} />
-// )
 
 interface Props {}
 
@@ -35,42 +31,10 @@ const PageFooter: React.FC<Props> = () => {
         <Text size={300} color="muted">© {new Date().getFullYear()}, Segment.io, Inc.</Text>
       </Pane>
       <Pane>
-        <Text size={300} color="muted">
-          Evergreen is an open source project developed at Segment.{' '}
-          <Link size={300} color="neutral" href="https://segment.com/jobs/" target="_blank">We’re hiring designers and engineers!</Link>
-        </Text>
+        <Link size={300} color="neutral" href="https://segment.com/jobs/" target="_blank" marginRight={majorScale(2)}>We're hiring!</Link>
+        <Link size={300} color="neutral" href="" onClick={openConsentManager}>Website Data Collection</Link>
       </Pane>
     </Pane>
-
-    // <footer className="PageFooter bg-dark clearfix">
-    //   <div className="Container PageFooter-inner">
-    //     <div className="PageFooter-left">
-    //       <SegmentLogoWordmark white width={100} height="auto" />
-    //     </div>
-    //     <div className="PageFooter-right">
-    //       <p>
-    //         Evergreen is an open source project developed at Segment.{' '}
-    //         <NativeLink
-    //           href="https://segment.com/jobs/"
-    //           className="focus-ring-link-white"
-    //         >
-    //           We’re hiring designers and engineers!
-    //         </NativeLink>
-    //       </p>
-    //     </div>
-    //   </div>
-    //   <div className="Container PageFooter-inner">
-    //     <div className="PageFooter-left">
-    //       <button type="button" onClick={openConsentManager}>
-    //         Website Data Collection Preferences
-    //       </button>
-    //     </div>
-
-    //     <div className="PageFooter-right">
-    //       <p>© {new Date().getFullYear()}, Segment.io, Inc.</p>
-    //     </div>
-    //   </div>
-    // </footer>
   )
 }
 
