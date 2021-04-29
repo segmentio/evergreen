691ab538cb3efa20f744395a36ebe46331c744ad
Allen Kleiner <akleiner24@gmail.com>
2021-04-29 14:28:52 -0700

    Add hero animation

--- docs-v2/components/homepage/HomeHero.tsx
+++ docs-v2/components/homepage/HomeHero.tsx
@@ -1,27 +1,62 @@
-import React from "react"
-import { Pane, useTheme, majorScale, Heading, Paragraph, Button, minorScale } from "evergreen-ui"
+import React, { useEffect, useRef } from 'react'
+
+import {
+  Pane,
+  useTheme,
+  majorScale,
+  Heading,
+  Paragraph,
+  Button,
+  minorScale
+} from 'evergreen-ui'
 
 const HomeHero = () => {
   const { colors } = useTheme() as any
+  const ref = useRef(null)
+
+  useEffect(() => {
+    import('@lottiefiles/lottie-player')
+  }, [])
 
   return (
     <Pane background={colors.gray50}>
       <Pane
-        paddingTop={majorScale(20)}
-        paddingBottom={majorScale(15)}
+        paddingY={majorScale(5)}
         marginX="auto"
         maxWidth={majorScale(143)}
         display="flex"
       >
-        <Pane>
+        <Pane
+          display="flex"
+          flexDirection="column"
+          alignItems="flex-start"
+          justifyContent="center"
+        >
           <Heading size={900}>Meet Evergreen,</Heading>
-          <Heading size={900} marginTop={minorScale(1)}>Segment’s design system</Heading>
-          <Paragraph size={500} marginTop={majorScale(2)} marginBottom={majorScale(3)}>
-            Evergreen is a React UI Framework for building ambitious products on the web. Brought to you by Segment.</Paragraph>
+          <Heading size={900} marginTop={minorScale(1)}>
+            Segment’s design system
+          </Heading>
+          <Paragraph
+            size={500}
+            marginTop={majorScale(2)}
+            marginBottom={majorScale(3)}
+          >
+            Evergreen is a React UI Framework for building ambitious products on
+            the web. Brought to you by Segment.
+          </Paragraph>
           <Button appearance="primary">Get Started</Button>
         </Pane>
         <Pane>
-          {/* TODO: Add animation mov or json file */}
+          <lottie-player
+            ref={ref}
+            style={{
+              width: '100%'
+            }}
+            src="https://assets3.lottiefiles.com/packages/lf20_dum5dwmu.json"
+            autoplay
+            loop
+            mode="normal"
+          />
         </Pane>
       </Pane>
     </Pane>
---
+++ docs-v2/types/@lottiefiles/lottie-player.d.ts
@@ -0,0 +1,5 @@
+declare namespace JSX {
+  interface IntrinsicElements {
+    "lottie-player": any;
+  }
+}
