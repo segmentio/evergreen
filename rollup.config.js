9f89cb39f53f402ad1ddf60d886a6b97b5143e15
Yue Yang <g1enyy0ung@gmail.com>
2021-06-17 11:01:54 +0800

    fix: resolve replace warning
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,9 @@
 import babel from '@rollup/plugin-babel'
 import commonjs from '@rollup/plugin-commonjs'
+import pkg from './package.json'
 import replace from '@rollup/plugin-replace'
 import resolve from '@rollup/plugin-node-resolve'
 import { terser } from 'rollup-plugin-terser'
-import pkg from './package.json'
 
 // Use peerDependencies as rollup external
 // https://rollupjs.org/guide/en/#peer-dependencies
@@ -32,8 +32,11 @@ export default [
     },
     plugins: [
       replace({
-        'process.env.NODE_ENV': JSON.stringify('development'),
-        ...ignoreSSR
+        preventAssignment: true,
+        values: {
+          'process.env.NODE_ENV': JSON.stringify('development'),
+          ...ignoreSSR
+        }
       }),
       resolve(),
       commonjs({
@@ -61,8 +64,11 @@ export default [
     },
     plugins: [
       replace({
-        'process.env.NODE_ENV': JSON.stringify('production'),
-        ...ignoreSSR
+        preventAssignment: true,
+        values: {
+          'process.env.NODE_ENV': JSON.stringify('production'),
+          ...ignoreSSR
+        }
       }),
       resolve(),
       commonjs({
