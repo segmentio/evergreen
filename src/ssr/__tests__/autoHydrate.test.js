41e7a8f5186a3efc93885f7dac40de77dfdeaa65
Matt Shwery <matthew.shwery@segment.com>
2021-07-15 21:12:18 -0500

    rm enzyme (#1263)

--- src/ssr/__tests__/autoHydrate.test.js
+++ src/ssr/__tests__/autoHydrate.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render } from 'enzyme'
+import { render } from '@testing-library/react'
 import Box from 'ui-box'
 import { hydrate } from '../src/autoHydrate'
 import extractStyles from '../src/extractStyles'
