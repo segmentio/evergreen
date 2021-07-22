8cb9e952660934fbf54ff8ce8a6220911baca03b
Yue Yang <g1enyy0ung@gmail.com>
2021-07-22 11:16:18 +0800

    chore: upgrade rollup (#1068)

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,14 @@
-import babel from 'rollup-plugin-babel'
+import babel from '@rollup/plugin-babel'
 import commonjs from '@rollup/plugin-commonjs'
+import pkg from './package.json'
 import replace from '@rollup/plugin-replace'
 import resolve from '@rollup/plugin-node-resolve'
 import { terser } from 'rollup-plugin-terser'
-import pkg from './package.json'
 
 // Use peerDependencies as rollup external
 // https://rollupjs.org/guide/en/#peer-dependencies
 const { peerDependencies } = pkg
 const external = Object.keys(peerDependencies)
-
-// Explicitly specify unresolvable named exports.
-// https://github.com/rollup/plugins/tree/master/packages/commonjs#namedexports
-const namedExports = {
-  'node_modules/react-is/index.js': ['isForwardRef'],
-  'node_modules/prop-types/index.js': [
-    'array',
-    'arrayOf',
-    'func',
-    'number',
-    'object',
-    'oneOf',
-    'oneOfType',
-    'string'
-  ]
-}
 
 // Ignore SSR imports in UMD, replace with empty functions
 const ignoreSSR = {
@@ -48,17 +32,19 @@ export default [
     },
     plugins: [
       replace({
-        'process.env.NODE_ENV': JSON.stringify('development'),
-        ...ignoreSSR
+        preventAssignment: true,
+        values: {
+          'process.env.NODE_ENV': JSON.stringify('development'),
+          ...ignoreSSR
+        }
       }),
       resolve(),
       commonjs({
-        include: 'node_modules/**',
-        namedExports
+        include: 'node_modules/**'
       }),
       babel({
         exclude: 'node_modules/**',
-        runtimeHelpers: true
+        babelHelpers: 'runtime'
       })
     ]
   },
@@ -78,17 +64,19 @@ export default [
     },
     plugins: [
       replace({
-        'process.env.NODE_ENV': JSON.stringify('production'),
-        ...ignoreSSR
+        preventAssignment: true,
+        values: {
+          'process.env.NODE_ENV': JSON.stringify('production'),
+          ...ignoreSSR
+        }
       }),
       resolve(),
       commonjs({
-        include: 'node_modules/**',
-        namedExports
+        include: 'node_modules/**'
       }),
       babel({
         exclude: 'node_modules/**',
-        runtimeHelpers: true
+        babelHelpers: 'runtime'
       }),
       terser()
     ]
