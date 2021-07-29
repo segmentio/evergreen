5474e11e6e99adc8568dd96016e20c3c33876cbd
Matt Shwery <mshwery@gmail.com>
2021-07-28 20:38:07 -0500

    Updates

---
+++ _next/static/Jn2CjrFUxhbBByohfq_xn/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Ffoundations\u002F[id]\u002Fprops","\u002Fcomponents\u002F[id]\u002Fprops","\u002Fpatterns\u002F[id]\u002Fprops","\u002Ffoundations\u002F[id]","\u002Fpatterns\u002F[id]","\u002Fintroduction\u002F[id]","\u002Fcomponents\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
