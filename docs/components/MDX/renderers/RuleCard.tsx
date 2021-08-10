9874236ee29595c3bbbefadafef84d95d1ef54b7
Kate Hsiao <kamebkj@gmail.com>
2021-08-10 14:22:36 -0700

    Add imageAlt prop to RuleCard

--- docs/components/MDX/renderers/RuleCard.tsx
+++ docs/components/MDX/renderers/RuleCard.tsx
@@ -4,17 +4,18 @@ import React from 'react'
 interface Props {
   type: string
   description: string
-  image: string
+  image?: string
+  imageAlt?: string
 }
 
-const RuleCard: React.FC<Props> = ({ type, description, image }) => {
+const RuleCard: React.FC<Props> = ({ type, description, image, imageAlt }) => {
   const color = type === 'Do' ? '#52BD94' : '#D14343'
   const icon = type === 'Do' ? <TickCircleIcon color={color} /> : <CrossIcon color={color} />
   return (
     <Card width="500px" border="default" marginRight={majorScale(5)} padding={majorScale(3)}>
       {image && (
         <Pane marginBottom={majorScale(2)}>
-          <img src={image} alt={image} width="100%" />
+          <img src={image} alt={imageAlt} width="100%" />
         </Pane>
       )}
       <Pane>
