12f6cbeb9816bea50c2463cc55f44a51d79b4628
EJ Mason <13525251+mxmason@users.noreply.github.com>
2021-09-20 18:43:01 -0700

    Add explainer comments

--- src/lib/generate-aria-describedby.js
+++ src/lib/generate-aria-describedby.js
@@ -1,9 +1,22 @@
+/**
+ * Generates a token valie for `aria-describedby`
+ * @param {String} id
+ * @param {Object} helperTextCandidates
+ * @param {String | React.ReactNode} [helperTextCandidates.description]
+ * @param {String | React.ReactNode} [helperTextCandidates.hint]
+ * @param {String | React.ReactNode} [helperTextCandidates.validationMessage]
+ * @returns {String | null}
+ */
 export function generateAriaDescribedBy(id, { description, hint, validationMessage }) {
   if (!id || (!description && !hint && !validationMessage)) {
     return null
   }
 
   let tokens = ''
+
+  // Only add each of the following to tokens if it is a non-empty string.
+  // When the user provides a React element, we assume they
+  // want to manage aria-describedby themselves.
 
   if (typeof description === 'string' && description.length) {
     tokens += ' ' + id + '__description'
