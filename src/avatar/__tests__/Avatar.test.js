2466959af672e6d6742e83b29869a6c81c114a18
Kate Hsiao <kamebkj@gmail.com>
2021-09-30 11:59:29 -0700

    basic and smoke tests

--- src/avatar/__tests__/Avatar.test.js
+++ src/avatar/__tests__/Avatar.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import Avatar from '../src/Avatar'
 
 describe('Avatar', () => {
@@ -10,34 +10,39 @@ describe('Avatar', () => {
   })
 
   it('should render question mark when name is empty and no image src', () => {
-    const { getByText } = render(<Avatar name="" />)
-    expect(getByText('?')).toBeVisible()
+    render(<Avatar name="" />)
+    expect(screen.getByText('?')).toBeVisible()
   })
 
   it('should render one char when name is one word and no image src', () => {
-    const { getByText } = render(<Avatar name="Alan" />)
-    expect(getByText('A')).toBeVisible()
+    render(<Avatar name="Alan" />)
+    expect(screen.getByText('A')).toBeVisible()
   })
 
   it('should render first and last initial when name is more than one word and no image src', () => {
-    const { getByText } = render(<Avatar name="Alan Turing Three" />)
-    expect(getByText('AT')).toBeVisible()
+    render(<Avatar name="Alan Turing Batman" />)
+    expect(screen.getByText('AT')).toBeVisible()
   })
 
   it('should render an image when pass in src', () => {
-    const { getByRole } = render(
-      <Avatar src="https://upload.wikimedia.org/wikipedia/commons/a/a1/Alan_Turing_Aged_16.jpg" />
+    render(<Avatar src="https://upload.wikimedia.org/wikipedia/commons/a/a1/Alan_Turing_Aged_16.jpg" />)
+    expect(screen.getByRole('img').getAttribute('src')).toEqual(
+      'https://upload.wikimedia.org/wikipedia/commons/a/a1/Alan_Turing_Aged_16.jpg'
     )
-    expect(
-      getByRole('img', { src: 'https://upload.wikimedia.org/wikipedia/commons/a/a1/Alan_Turing_Aged_16.jpg' })
-    ).toBeVisible()
   })
 
   it('should render the according size', () => {
-    expect()
+    render(<Avatar name="Alan Turing" size={80} />)
+    expect(screen.getByTitle('Alan Turing')).toHaveStyle('height: 80px')
   })
 
   it('should render a square if the shape is square', () => {
-    expect()
+    render(<Avatar name="Alan Turing" shape="square" />)
+    expect(screen.getByTitle('Alan Turing')).toHaveStyle('border-top-right-radius: 4px')
+  })
+
+  it('should render the color when specified', () => {
+    render(<Avatar name="Alan Turing" color="green" />)
+    expect(screen.getByTitle('Alan Turing')).toHaveStyle('background-color: #DCF2EA')
   })
 })
