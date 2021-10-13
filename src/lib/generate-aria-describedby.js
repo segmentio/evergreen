4f2515b8452c51096c8579948515a50704f7fc4f
EJ Mason <13525251+mxmason@users.noreply.github.com>
2021-10-13 03:05:57 -0700

    Reorder describedby tokens

--- src/lib/generate-aria-describedby.js
+++ src/lib/generate-aria-describedby.js
@@ -14,16 +14,19 @@ export function generateAriaDescribedBy(id, { description, hint, validationMessa
   // it was provided as a string. When the author provides
   // a React element, we assume they want to manage aria-describedby themselves.
 
+  // NB: Token order matches the order that these hint texts appear in the DOM.
+  // If the order of the hint texts changes, this should also change.
+
   if (typeof description === 'string') {
     tokens += ' ' + id + '__description'
   }
 
-  if (typeof hint === 'string') {
-    tokens += ' ' + id + '__hint'
-  }
-
   if (typeof validationMessage === 'string') {
     tokens += ' ' + id + '__validation-message'
+  }
+
+  if (typeof hint === 'string') {
+    tokens += ' ' + id + '__hint'
   }
 
   if (tokens.length) {
