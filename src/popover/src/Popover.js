fb60e188d2380d8a93185c53c80c00c19f241dd1
Jon Luo <64451308+jon-quin@users.noreply.github.com>
2022-02-07 13:59:58 -0800

    [Fix] Popover imperative open/close can break due to dependency array. (#1397)

--- src/popover/src/Popover.js
+++ src/popover/src/Popover.js
@@ -41,15 +41,6 @@ const Popover = memo(
     const targetRef = useRef()
     const setTargetRef = useMergedRef(targetRef)
 
-    useImperativeHandle(
-      forwardedRef,
-      () => ({
-        open,
-        close
-      }),
-      [popoverNode.current]
-    )
-
     /**
      * Methods borrowed from BlueprintJS
      * https://github.com/palantir/blueprint/blob/release/2.0.0/packages/core/src/components/overlay/overlay.tsx
@@ -137,6 +128,15 @@ const Popover = memo(
       bringFocusBackToTarget()
       onClose()
     }, [setIsShown, bringFocusBackToTarget, onClose, isShown])
+
+    useImperativeHandle(
+      forwardedRef,
+      () => ({
+        open,
+        close
+      }),
+      [open, close]
+    )
 
     // If `props.isShown` is a boolean, treat as a controlled component
     // `open` and `close` should be applied when it changes
