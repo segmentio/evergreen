8f761b2f8a0d9cc292ddde554bff3a0f8b5d2a1f
Brandon Scott <contact@brandonscott.me>
2022-02-23 16:32:15 -0500

    Add additional FileCard stories for different states

--- src/file-uploader/stories/index.stories.js
+++ src/file-uploader/stories/index.stories.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import Box from 'ui-box'
+import { MimeType } from '../../constants'
 import { FileUploader, FileCard } from '../../file-uploader'
+import { toaster } from '../../toaster'
+
+const handleRemove = () => toaster.notify('Removed file!')
 
 storiesOf('file-uploader', module)
   .add('FileUploader', () => (
@@ -19,13 +23,28 @@ storiesOf('file-uploader', module)
         document.body.style.margin = '0'
         document.body.style.height = '100vh'
       })()}
-      <FileCard name="Sample-Image.png" type="image/png" sizeInBytes={0.75 * 1024 * 1024} />
+      <FileCard name="Sample-Image.png" sizeInBytes={0.75 * 1024 * 1024} type={MimeType.png} />
       <FileCard
-        name="Sample-Image.gif"
-        type="image/gif"
-        sizeInBytes={3 * 1024 * 1024}
         isInvalid={true}
+        name="Sample-Image.gif"
+        sizeInBytes={3 * 1024 * 1024}
+        type={MimeType.gif}
         validationMessage="This file is not an accepted format. You can upload .png and .pdf file formats."
+      />
+      <FileCard
+        name="favicon.ico"
+        onRemove={handleRemove}
+        sizeInBytes={128 * 1024}
+        src="https://segment.com/favicon.ico"
+        type={MimeType.ico}
+      />
+      <FileCard
+        description="Uploading..."
+        isLoading={true}
+        name="Sample-Image.png"
+        onRemove={handleRemove}
+        sizeInBytes={0.75 * 1024 * 1024}
+        type={MimeType.png}
       />
     </Box>
   ))
