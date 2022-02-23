3ddbfd38759d3bf1b86066218a847458621e2442
Brandon Scott <contact@brandonscott.me>
2022-02-23 11:19:41 -0500

    Add MimeType constant

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,4 @@
 export { default as StackingOrder } from './src/StackingOrder'
 export { default as Intent } from './src/Intent'
 export { default as Position } from './src/Position'
+export { default as MimeType } from './src/MimeType'
