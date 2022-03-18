c52484be98f36e10bc3e4915a395e57570454757
Brandon Scott <contact@brandonscott.me>
2022-03-18 10:50:48 -0400

    Not sure what happened but useStyleConfig changes got wiped out. Restored from VS Code history

--- src/hooks/use-style-config.ts
+++ src/hooks/use-style-config.ts
@@ -1,39 +1,28 @@
 import { useMemo, useRef } from 'react'
-import { css } from 'glamor'
-// @ts-expect-error ts-migrate(7016) FIXME: Could not find a declaration file for module 'loda... Remove this comment to see the full error message
+import { css, CSSProperties } from 'glamor'
 import merge from 'lodash.merge'
 import isEqual from 'react-fast-compare'
-import { splitBoxProps } from 'ui-box'
+import { EnhancerProps, splitBoxProps } from 'ui-box'
 import { useTheme, get, resolveThemeTokens } from '../theme'
+import { Appearance } from '../types/theme/appearance'
+import { ComponentStyle } from '../types/theme/component-style'
+import { Components } from '../types/theme/components'
+import { IntentTypes } from '../types/theme/intent-types'
+import { PseudoSelectorKey, PseudoSelectors } from '../types/theme/pseudo-selectors'
+import { Size } from '../types/theme/size'
 
-/**
- * @typedef {object} StateStyles
- * @property {import('csstype').Properties} [_hover]
- * @property {import('csstype').Properties} [_active]
- * @property {import('csstype').Properties} [_disabled]
- * @property {import('csstype').Properties} [_focus]
- * @property {import('csstype').Properties} [_invalid]
- */
+type GlamorAndBoxStyle = EnhancerProps & CSSProperties
+interface StyleModifiers {
+  appearance?: Appearance
+  intent?: IntentTypes
+  size?: Size
+}
 
-/**
- * @typedef {{ [placeholder in keyof StateStyles]: string }} PseudoSelectors
- */
+interface UseGlamorAndBoxOutput extends EnhancerProps {
+  className: string
+}
 
-/**
- * @typedef {object} StyleModifiers
- * @property {string} [appearance]
- * @property {string} [size]
- */
-
-/** @typedef {import('ui-box').EnhancerProps & StateStyles} Style */
-/** @typedef {import('ui-box').EnhancerProps & import('glamor').CSSProperties} GlamorAndBoxStyle */
-
-/**
- * @typedef {object} StyleConfig
- * @property {Style} baseStyle
- * @property {{ [appearance: string]: Style }} [appearances]
- * @property {{ [size: string]: Style }} [sizes]
- */
+type UseStyleConfigOutput = UseGlamorAndBoxOutput
 
 function maybeRun(value: any, ...args: any[]) {
   return typeof value === 'function' ? value(...args) : value
@@ -41,10 +30,9 @@ function maybeRun(value: any, ...args: any[]) {
 
 function maybeRunDeep(raw: any, ...args: any[]) {
   if (raw && typeof raw === 'object') {
-    const result = {}
+    const result: Record<string, any> = {}
 
     for (const key of Object.keys(raw)) {
-      // @ts-expect-error ts-migrate(7053) FIXME: Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
       result[key] = maybeRunDeep(raw[key], ...args)
     }
 
@@ -56,17 +44,16 @@ function maybeRunDeep(raw: any, ...args: any[]) {
 
 /**
  * Combines styles from a styleConfig, with the given style modifiers (appearance, size, etc) and internal styles
- * @param {object} theme
- * @param {StyleModifiers} props
- * @param {StyleConfig} styleConfig
- * @param {GlamorAndBoxStyle} [internalStyles]
- * @returns {StyleConfig}
  */
-function combineStyles(theme: any, props: any, styleConfig: any, internalStyles = {}) {
+function combineStyles(
+  theme: any,
+  props: StyleModifiers,
+  styleConfig: ComponentStyle,
+  internalStyles: GlamorAndBoxStyle = {}
+): ComponentStyle {
   const config = maybeRun(styleConfig, theme, props)
   const baseStyle = maybeRunDeep(config.baseStyle, theme, props)
   const sizeStyle = maybeRunDeep(get(config, `sizes.${props.size}`, {}), theme, props)
-
   const appearanceStyle = maybeRunDeep(get(config, `appearances.${props.appearance}`, {}), theme, props)
 
   return merge({}, internalStyles, baseStyle, sizeStyle, appearanceStyle)
@@ -75,9 +62,13 @@ function combineStyles(theme: any, props: any, styleConfig: any, internalStyles 
 /**
  * Combines a styleConfig object with internal styles based on the theme + style modifiers (props)
  * and returns a memoized style object
- * @returns {StyleConfig}
  */
-function useMergedStyles(theme: any, props: any, styleConfig: any, internalStyles: any) {
+function useMergedStyles(
+  theme: any,
+  props: StyleModifiers,
+  styleConfig: ComponentStyle,
+  internalStyles: GlamorAndBoxStyle
+): ComponentStyle {
   const styleRef = useRef({})
 
   return useMemo(() => {
@@ -93,34 +84,32 @@ function useMergedStyles(theme: any, props: any, styleConfig: any, internalStyle
 /**
  * Split up the style props into glamor-ready and box-ready props (className + spreadable props)
  */
-function useGlamorAndBox(styles: any, pseudoSelectors: any) {
+function useGlamorAndBox(styles: any, pseudoSelectors: PseudoSelectors): UseGlamorAndBoxOutput {
   const glamorStylesRef = useRef({})
-  const classNameRef = useRef()
+  const classNameRef = useRef<string | undefined>()
 
   return useMemo(() => {
     // Split the resulting style object into ui-box-compatible props and the rest
     const { matchedProps, remainingProps } = splitBoxProps(styles)
 
-    /** @type {GlamorAndBoxStyle} */
-    const glamorStyles = {}
+    const glamorStyles: GlamorAndBoxStyle = {}
 
     // Swap out pseudo selector placeholders for their actual css selector strings
-    for (const k of Object.keys(remainingProps)) {
-      const key = k in pseudoSelectors ? pseudoSelectors[k] : k
-      // @ts-expect-error ts-migrate(7053) FIXME: Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
-      glamorStyles[key] = remainingProps[k]
+    for (const placeholderKey of Object.keys(remainingProps)) {
+      const key =
+        placeholderKey in pseudoSelectors ? pseudoSelectors[placeholderKey as PseudoSelectorKey] : placeholderKey
+      glamorStyles[key] = remainingProps[placeholderKey]
     }
 
     // Take all the "non-compatible" props and give those to glamor (since ui-box doesn't know how to handle them yet)
     if (!isEqual(glamorStylesRef.current, glamorStyles)) {
       const className = css(glamorStyles).toString()
       glamorStylesRef.current = glamorStyles
-      // @ts-expect-error ts-migrate(2322) FIXME: Type 'string | undefined' is not assignable to typ... Remove this comment to see the full error message
       classNameRef.current = className === 'css-nil' ? undefined : className
     }
 
     return {
-      className: classNameRef.current,
+      className: classNameRef.current as string,
       ...matchedProps
     }
   }, [styles, pseudoSelectors])
@@ -128,18 +117,21 @@ function useGlamorAndBox(styles: any, pseudoSelectors: any) {
 
 /**
  * Takes a styleConfig object and outputs a `className` and `boxProps` that can be spread on a Box component
- * @param {string} componentKey the name of the component in the theme
- * @param {StyleModifiers} props props that modify the resulting visual style (e.g. `size` or `appearance`)
- * @param {PseudoSelectors} pseudoSelectors mapping for the component between states and actual pseudo selectors
- * @param {GlamorAndBoxStyle} [internalStyles] additional styles that are specified internally, separate from the visual styles
- * @returns {{ className: string; boxProps: import('ui-box').EnhancerProps }}
+ * @param componentKey the name of the component in the theme
+ * @param props props that modify the resulting visual style (e.g. `size` or `appearance`)
+ * @param pseudoSelectors mapping for the component between states and actual pseudo selectors
+ * @param additional styles that are specified internally, separate from the visual styles
  */
-export function useStyleConfig(componentKey: any, props: any, pseudoSelectors: any, internalStyles: any) {
+export function useStyleConfig<T extends Components>(
+  componentKey: T,
+  props: StyleModifiers,
+  pseudoSelectors: PseudoSelectors,
+  internalStyles: GlamorAndBoxStyle
+): UseStyleConfigOutput {
   const theme = useTheme()
 
   // Get the component style object from the theme
-  // @ts-expect-error ts-migrate(2554) FIXME: Expected 3 arguments, but got 2.
-  const componentStyles = get(theme, `components.${componentKey}`) || {}
+  const componentStyles = get(theme, `components.${componentKey}`, {})
 
   // Merges the theme styles with the modifiers/props (appearance, size, etc)
   const mergedStyles = useMergedStyles(theme, props, componentStyles, internalStyles)
