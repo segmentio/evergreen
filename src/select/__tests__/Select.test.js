02a0611ef9fc4913090b2286a3970218f4bda4b1
Brandon Scott <contact@brandonscott.me>
2022-04-10 14:31:21 -0400

    Add mockRef test util, separate out TextInput/TextInputField tests

--- src/select/__tests__/Select.test.js
+++ src/select/__tests__/Select.test.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import { Select } from '../'
+import { mockRef } from '../../test/utils'
 
 const makeSelectFixture = (props = {}) => <Select data-testid="select" {...props} />
 
 describe('Select', () => {
   it('should forward ref to underlying <select />', () => {
-    const ref = {
-      current: jest.fn()
-    }
+    const ref = mockRef()
 
     render(makeSelectFixture({ ref }))
 
