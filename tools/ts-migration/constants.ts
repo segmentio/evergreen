5e14f5d84a9c5becabbf55a51d4ee5872119b3b0
Brandon Scott <contact@brandonscott.me>
2022-05-03 07:57:41 -0400

    Remove ts-migration utils + ts-morph

--- tools/ts-migration/constants.ts
+++
@@ -1,4 +0,0 @@
-const ICON_COMPONENT = 'IconComponent'
-const INDEX_D_TS = 'index.d.ts'
-
-export { ICON_COMPONENT, INDEX_D_TS }
--- tools/ts-migration/log.ts
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line no-console
-const info = (message: string, ...values: any[]) => console.log(message, ...values)
-
-const log = {
-  info
-}
-
-export { log }
