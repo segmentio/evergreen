7ca4661b5cdb77d1a37ff4091ee1c45e18b63377
Shreedhar Bhat <shridharbhat1998@gmail.com>
2022-05-14 19:40:30 +0530

    refactored and moved DialogBody

--- src/dialog-body/src/DialogBody.js
+++ src/dialog-body/src/DialogBody.js
@@ -1,19 +1,74 @@
 import React, { memo, forwardRef } from 'react'
 import PropTypes from 'prop-types'
-import Box from 'ui-box'
+import { useStyleConfig } from '../../hooks'
+import { Pane } from '../../layers'
+import { Paragraph } from '../../typography'
 
-const DialogBody = memo(forwardRef((props, ref) => {
-  const { ...restProps } = props
+const closeHandler = close => close()
 
-  return (
-    <Box ref={ref} {...restProps}>
-      DialogBody
-    </Box>
-  )
-}))
+const renderChildren = (children, close) => {
+  if (typeof children === 'function') {
+    return children({ close })
+  }
+
+  if (typeof children === 'string') {
+    return <Paragraph>{children}</Paragraph>
+  }
+
+  return children
+}
+
+const DialogBody = memo(
+  forwardRef(function DialogBody(props, ref) {
+    const emptyProps = {}
+
+    const themedBodyProps = useStyleConfig('DialogBody', emptyProps, emptyProps, emptyProps)
+
+    const { contentContainerProps, minHeightContent = 80, onCancel = closeHandler, state, children } = props
+
+    return (
+      <Pane
+        ref={ref}
+        data-state={state}
+        display="flex"
+        overflow="auto"
+        flexDirection="column"
+        minHeight={minHeightContent}
+        {...themedBodyProps}
+        {...contentContainerProps}
+      >
+        <Pane>{renderChildren(children, onCancel)}</Pane>
+      </Pane>
+    )
+  })
+)
 
 DialogBody.propTypes = {
-
+  /**
+   * Children can be a string, node or a function accepting `({ close })`.
+   * When passing a string, <Paragraph /> is used to wrap the string.
+   */
+  children: PropTypes.oneOfType([PropTypes.node, PropTypes.func]).isRequired,
+  /**
+   * Props that are passed to the content container.
+   */
+  contentContainerProps: PropTypes.object,
+  /**
+   * The min height of the body content.
+   * Makes it less weird when only showing little content.
+   */
+  minHeightContent: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  /**
+   * Function that will be called when the cancel button is clicked.
+   * This closes the Dialog by default.
+   *
+   * `onCancel={(close) => close()}`
+   */
+  onCancel: PropTypes.func,
+  /**
+   * State of the dialog passed to dialog.
+   */
+  state: PropTypes.string
 }
 
 export default DialogBody
--- src/dialog-body/stories/index.stories.js
+++ src/dialog-body/stories/index.stories.js
@@ -1,15 +1,16 @@
-import { storiesOf } from '@storybook/react'
 import React from 'react'
+import { storiesOf } from '@storybook/react'
 import Box from 'ui-box'
 import { DialogBody } from '../../dialog-body'
 
-storiesOf('dialog-body', module)
-  .add('DialogBody', () => (
-    <Box padding={40}>
-      {(() => {
-        document.body.style.margin = '0'
-        document.body.style.height = '100vh'
-      })()}
-      <DialogBody>DialogBody</DialogBody>
-    </Box>
-  ))
+storiesOf('dialog-body', module).add('DialogBody', () => (
+  <Box padding={40}>
+    {(() => {
+      document.body.style.margin = '0'
+      document.body.style.height = '100vh'
+    })()}
+    <DialogBody state="dialog body" onCancel={close}>
+      Its a example of DialogBody
+    </DialogBody>
+  </Box>
+))
