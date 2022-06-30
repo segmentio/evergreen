41c0d710983f23abf4c6d6a90e18d1bdbb277eb2
Joe Fehrman <joseph.fehrman@gmail.com>
2022-06-30 09:43:28 -0400

    Unrequired length check in autocomplete (#1489)

--- src/autocomplete/src/Autocomplete.js
+++ src/autocomplete/src/Autocomplete.js
@@ -48,7 +48,7 @@ const AutocompleteItems = ({
   itemsFilter = itemsFilter || fuzzyFilter(itemToString)
   const items = isFilterDisabled || inputValue.trim() === '' ? originalItems : itemsFilter(originalItems, inputValue)
 
-  if (items.length === 0) return null
+  if (items.length <= 0) return null
 
   // Pass the actual DOM ref to downshift, this fixes touch support
   const menuProps = getMenuProps()
@@ -62,33 +62,31 @@ const AutocompleteItems = ({
           </Text>
         </Pane>
       )}
-      {items.length > 0 && (
-        <VirtualList
-          width="100%"
-          height={Math.min(items.length * itemSize, popoverMaxHeight)}
-          itemSize={itemSize}
-          itemCount={items.length}
-          scrollToIndex={highlightedIndex || 0}
-          overscanCount={3}
-          scrollToAlignment="auto"
-          renderItem={({ index, style }) => {
-            const item = items[index]
-            const itemString = itemToString(item)
+      <VirtualList
+        width="100%"
+        height={Math.min(items.length * itemSize, popoverMaxHeight)}
+        itemSize={itemSize}
+        itemCount={items.length}
+        scrollToIndex={highlightedIndex || 0}
+        overscanCount={3}
+        scrollToAlignment="auto"
+        renderItem={({ index, style }) => {
+          const item = items[index]
+          const itemString = itemToString(item)
 
-            return renderItem(
-              getItemProps({
-                item,
-                key: itemString,
-                index,
-                style,
-                children: itemString,
-                isSelected: itemToString(selectedItem) === itemString,
-                isHighlighted: highlightedIndex === index
-              })
-            )
-          }}
-        />
-      )}
+          return renderItem(
+            getItemProps({
+              item,
+              key: itemString,
+              index,
+              style,
+              children: itemString,
+              isSelected: itemToString(selectedItem) === itemString,
+              isHighlighted: highlightedIndex === index
+            })
+          )
+        }}
+      />
     </Pane>
   )
 }
