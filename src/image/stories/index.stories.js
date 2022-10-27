a18f11095f69011511f45b3b67bdd074fb649a63
Quentin Neal <quentinneal@outlook.com>
2022-10-26 17:09:27 -0700

    Changed image storybook url. Previous link doesn't work.

--- src/image/stories/index.stories.js
+++ src/image/stories/index.stories.js
@@ -9,6 +9,6 @@ storiesOf('image', module).add('Image', () => (
       document.body.style.margin = '0'
       document.body.style.height = '100vh'
     })()}
-    <Image src="http://lorempixel.com/output/cats-q-c-640-480-5.jpg" />
+    <Image src="http://placekitten.com/640/480" />
   </Box>
 ))
