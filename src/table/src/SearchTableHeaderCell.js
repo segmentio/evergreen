5992322ae07cb2c6805551a5c08e7f8510d0923c
Brandon Scott <contact@brandonscott.me>
2022-11-07 10:40:41 -0500

    Add back font smoothing style after side-by-side comparison of Storybook

--- src/table/src/SearchTableHeaderCell.js
+++ src/table/src/SearchTableHeaderCell.js
@@ -7,6 +7,15 @@ import TableHeaderCell from './TableHeaderCell'
 
 const noop = () => {}
 
+/**
+ * This prop is non-standard, macOS specific and unsupported by ui-box. We probably don't need it,
+ * but retaining it for backwards compatibility
+ * @see https://developer.mozilla.org/en-US/docs/Web/CSS/font-smooth
+ */
+const style = {
+  '-webkit-font-smoothing': 'antialiased'
+}
+
 const SearchTableHeaderCell = memo(
   forwardRef(function SearchTableHeaderCell(props, ref) {
     const {
@@ -32,6 +41,7 @@ const SearchTableHeaderCell = memo(
           border="none"
           backgroundColor="transparent"
           appearance="none"
+          style={style}
           selectors={{
             '&:focus': {
               outline: 'none'
