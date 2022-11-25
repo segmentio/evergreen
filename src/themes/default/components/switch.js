a096757db9a9433b2d9564c7d213dcaf660137ba
Brandon Scott <contact@brandonscott.me>
2022-11-24 19:59:12 -0500

    Convert Switch theme

--- src/themes/default/components/switch.js
+++ src/themes/default/components/switch.js
@@ -2,36 +2,38 @@ const baseStyle = {}
 
 const appearances = {
   default: {
-    _base: {
-      color: 'white',
-      backgroundColor: 'colors.gray400'
-    },
-    _disabled: {
-      cursor: 'not-allowed',
-      opacity: 0.5
-    },
-    _hover: {
-      backgroundColor: 'colors.gray500'
-    },
-    _focus: {
-      boxShadow: theme => `0 0 0 3px ${theme.colors.blue100}`
-    },
-    _active: {
-      backgroundColor: 'colors.gray600'
-    },
-    _checked: {
-      backgroundColor: 'colors.blue500',
-      color: 'white'
-    },
-    _checkedHover: {
-      backgroundColor: 'colors.blue600',
-      color: 'white'
-    },
-    _checkedActive: {
-      backgroundColor: 'colors.blue700',
-      color: 'white'
-    },
-    _checkedDisabled: {}
+    selectors: {
+      _base: {
+        color: 'white',
+        backgroundColor: 'colors.gray400'
+      },
+      _disabled: {
+        cursor: 'not-allowed',
+        opacity: 0.5
+      },
+      _hover: {
+        backgroundColor: 'colors.gray500'
+      },
+      _focus: {
+        boxShadow: theme => `0 0 0 3px ${theme.colors.blue100}`
+      },
+      _active: {
+        backgroundColor: 'colors.gray600'
+      },
+      _checked: {
+        backgroundColor: 'colors.blue500',
+        color: 'white'
+      },
+      _checkedHover: {
+        backgroundColor: 'colors.blue600',
+        color: 'white'
+      },
+      _checkedActive: {
+        backgroundColor: 'colors.blue700',
+        color: 'white'
+      },
+      _checkedDisabled: {}
+    }
   }
 }
 
