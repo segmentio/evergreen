e2a26a11438ef7572b15ddcce4a4d426c32d249e
Brandon Scott <contact@brandonscott.me>
2023-01-05 12:42:06 -0500

    Remove duplicated TextInputField tests + refactor to use screen object from rtl

--- src/text-input/__tests__/TextInput.test.js
+++ src/text-input/__tests__/TextInput.test.js
@@ -1,15 +1,11 @@
 import React, { useState } from 'react'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import { TextInput, TextInputField } from '../'
+import { TextInput } from '../'
 import { mockRef } from '../../test/utils'
 
 function makeTextInputFixture(props = {}) {
   return <TextInput data-testid="input" {...props} />
-}
-
-function makeTextInputFieldFixture(props = {}) {
-  return <TextInputField data-testid="input" label="Name" {...props} />
 }
 
 describe('TextInput', () => {
@@ -26,16 +22,15 @@ describe('TextInput', () => {
   })
 
   it('should accept placeholder text', () => {
-    const { getByPlaceholderText } = render(makeTextInputFixture({ placeholder: 'Enter text here' }))
+    render(makeTextInputFixture({ placeholder: 'Enter text here' }))
 
-    expect(getByPlaceholderText('Enter text here')).toBeInTheDocument()
+    expect(screen.getByPlaceholderText('Enter text here')).toBeInTheDocument()
   })
 
   it('should set an invalid state if `isInvalid` is `true`', () => {
-    const { getByTestId } = render(makeTextInputFixture({ isInvalid: true }))
-    const input = getByTestId('input')
+    render(makeTextInputFixture({ isInvalid: true }))
 
-    expect(input).toHaveAttribute('aria-invalid', 'true')
+    expect(screen.getByTestId('input')).toHaveAttribute('aria-invalid', 'true')
   })
 
   it('should accept an `onChange` handler to be a controlled component', () => {
@@ -51,60 +46,21 @@ describe('TextInput', () => {
       )
     }
 
-    const { getByDisplayValue, getByTestId } = render(<ControlledTextInput />)
-    const input = getByTestId('input')
+    render(<ControlledTextInput />)
+    const input = screen.getByTestId('input')
     userEvent.click(input)
 
     expect(document.activeElement).toEqual(input)
     userEvent.type(input, 'Testing')
-    expect(getByDisplayValue('Testing')).toEqual(input)
+    expect(screen.getByDisplayValue('Testing')).toEqual(input)
   })
 
   it('should not be interactive if `disabled` is passed in', () => {
-    const { getByDisplayValue, getByTestId } = render(makeTextInputFixture({ disabled: true }))
-    const input = getByTestId('input')
+    render(makeTextInputFixture({ disabled: true }))
+    const input = screen.getByTestId('input')
     userEvent.type(input, 'Testing')
 
-    expect(() => getByDisplayValue('Testing')).toThrowError()
-    expect(getByDisplayValue('')).toEqual(input)
-  })
-})
-
-describe('TextInputField', () => {
-  it('Should render without crashing', () => {
-    expect(() => render(makeTextInputFieldFixture())).not.toThrow()
-  })
-
-  it('Should have expected accessible name when `label` prop', () => {
-    const { getByLabelText, getByTestId } = render(makeTextInputFieldFixture())
-    expect(getByLabelText('Name')).toBeInTheDocument()
-    expect(getByTestId('input')).toHaveAccessibleName('Name')
-  })
-
-  it('Should add hint text to accessible description when `hint` prop provided', () => {
-    const { getByTestId, getByText } = render(makeTextInputFieldFixture({ hint: 'Enter a value in the input' }))
-    expect(getByText('Enter a value in the input')).toBeInTheDocument()
-    expect(getByTestId('input')).toHaveAccessibleDescription('Enter a value in the input')
-  })
-
-  it('Should render an astrix when `required` is passed in', () => {
-    const { getByTitle } = render(makeTextInputFieldFixture({ required: true }))
-    expect(getByTitle('This field is required.')).toBeInTheDocument()
-  })
-
-  it('Should render a `validationMessage` when passed in', () => {
-    const { getByTestId, getByText } = render(makeTextInputFieldFixture({ validationMessage: 'Please enter a value.' }))
-    expect(getByText('Please enter a value.')).toBeInTheDocument()
-    expect(getByTestId('input')).toHaveAccessibleDescription('Please enter a value.')
-  })
-
-  it('Should correctly compose an accessible description from multiple hints', () => {
-    const { getByTestId, getByText } = render(
-      makeTextInputFieldFixture({ description: 'A description.', hint: 'Am hint.', validationMessage: 'Try again.' })
-    )
-    expect(getByText('A description.')).toBeInTheDocument()
-    expect(getByText('Am hint.')).toBeInTheDocument()
-    expect(getByText('Try again.')).toBeInTheDocument()
-    expect(getByTestId('input')).toHaveAccessibleDescription('A description. Try again. Am hint.')
+    expect(() => screen.getByDisplayValue('Testing')).toThrowError()
+    expect(screen.getByDisplayValue('')).toEqual(input)
   })
 })
