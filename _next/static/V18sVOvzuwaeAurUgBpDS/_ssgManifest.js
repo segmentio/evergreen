d86a357bda968350f2f2041bfc679f72e3272734
Brandon Scott <bscott@twilio.com>
2023-03-08 08:16:27 -0500

    Updates

--- _next/static/V18sVOvzuwaeAurUgBpDS/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Ffoundations\u002F[id]\u002Fprops","\u002Fpatterns\u002F[id]\u002Fprops","\u002Fcomponents\u002F[id]\u002Fprops","\u002Fpatterns\u002F[id]","\u002Fintroduction\u002Fmigrations\u002F[id]","\u002Ffoundations\u002F[id]","\u002Fcomponents\u002F[id]","\u002Fintroduction\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
